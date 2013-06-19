e8aec1954a4c2cbffb9b5f92245be2dfe4098f4a
Julie <ju.ralph@gmail.com>
2013-06-19 16:09:04 -0700

    Improving the example test specs, and fixing the jasmine one so it actually runs.

--- example/onMocha.js
+++ example/onMocha.js
@@ -1,10 +1,17 @@
+/**
+ * This example shows how to use the protractor library in a Mocha test.
+ * It assumes that a selenium server is running at localhost:4444.
+ * Run this test with:
+ *   mocha onMocha.js
+ */
+
 var util = require('util');
 var expect = require('expect.js');
 var webdriver = require('selenium-webdriver');
 var protractor = require('../lib/protractor.js');
 
-describe('angularjs homepage', function() {
-  this.timeout(8000);
+describe('angularjs.org homepage', function() {
+  this.timeout(80000);
 
   var driver, ptor;
 
@@ -29,39 +36,39 @@ describe('angularjs homepage', function() {
   it('should greet using binding', function(done) {
     ptor.get('http://www.angularjs.org');
 
-    ptor.findElement(protractor.By.input("yourName")).sendKeys("Julie");
+    ptor.findElement(protractor.By.input('yourName')).sendKeys('Julie');
 
-    ptor.findElement(protractor.By.binding("Hello {{yourName}}!")).
+    ptor.findElement(protractor.By.binding('{{yourName}}')).
         getText().then(function(text) {
           expect(text).to.eql('Hello Julie!');
           done();
         });
   });
 
-  it('should greet using binding - #2', function(done) {
+  it('should list todos', function(done) {
     ptor.get('http://www.angularjs.org');
 
-    ptor.findElement(protractor.By.input("yourName")).sendKeys("Jane");
+    var todo = ptor.findElement(
+        protractor.By.repeater('todo in todos').row(2));
 
-    ptor.findElement(protractor.By.binding("Hello {{yourName}}!")).
-        getText().then(function(text) {
-          expect(text).to.eql('Hello Jane!');
-          done();
-        });
+    todo.getText().then(function(text) {
+      expect(text).to.eql('build an angular app');
+      done();
+    });
   });
 
   // Uncomment to see failures.
-  /*
-  it('should greet using binding - this one fails', function(done) {
-    ptor.get('http://www.angularjs.org');
+  
+  // it('should greet using binding - this one fails', function(done) {
+  //   ptor.get('http://www.angularjs.org');
 
-    ptor.findElement(protractor.By.input("yourName")).sendKeys("Julie");
+  //   ptor.findElement(protractor.By.input("yourName")).sendKeys("Julie");
 
-    ptor.findElement(protractor.By.binding("Hello {{yourName}}!")).
-        getText().then(function(text) {
-          expect(text).to.eql('Hello Jack');
-          done();
-        });
-  });
-  */
+  //   ptor.findElement(protractor.By.binding("Hello {{yourName}}!")).
+  //       getText().then(function(text) {
+  //         expect(text).to.eql('Hello Jack');
+  //         done();
+  //       });
+  // });
+  
 });
