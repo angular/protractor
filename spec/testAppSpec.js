33c2c9aac3b9c59596ccc1f48c725b84e5b967ec
Julie <ju.ralph@gmail.com>
2013-07-22 14:38:49 -0700

    Adding tests for the various types of slowness that might result in the view getting out of sync.

--- spec/testAppSpec.js
+++ spec/testAppSpec.js
@@ -1,15 +1,26 @@
-var webptor = require('selenium-webdriver');
+var webdriver = require('selenium-webdriver');
 var protractor = require('../lib/protractor.js');
 var util = require('util');
 require('../jasminewd');
 
 describe('test application', function() {
   var ptor;
+
+  describe('no ptor at all', function() {
+    it('should still do normal tests', function() {
+      expect(true).toBe(true);
+    });
+  });
+
   describe('finding elements in forms', function() {
     ptor = protractor.getInstance();
 
     beforeEach(function() {
       ptor.get('app/index.html#/form');
+    });
+
+    it('should still do normal tests', function() {
+      expect(true).toBe(true);
     });
 
     it('should find an element by binding', function() {
@@ -352,4 +363,97 @@ describe('protractor library', function() {
         expect(url).toEqual('http://localhost:8000/app/index.html#/http');
       });
     });
+});
+
+describe('synchronizing with slow pages', function() {
+  var ptor = protractor.getInstance();
+
+  beforeEach(function() {
+    ptor.get('app/index.html#/async');
+  });
+
+  it('waits for http calls', function() {
+    var status =
+      ptor.findElement(protractor.By.binding('slowHttpStatus'));
+    var button = ptor.findElement(protractor.By.css('[ng-click="slowHttp()"]'));
+
+    expect(status.getText()).toEqual('not started');
+
+    button.click();
+    ptor.waitForAngular();
+
+    expect(status.getText()).toEqual('done');
+  });
+
+  it('waits for long javascript execution', function() {
+    var status =
+      ptor.findElement(protractor.By.binding('slowFunctionStatus'));
+    var button =
+      ptor.findElement(protractor.By.css('[ng-click="slowFunction()"]'));
+
+    expect(status.getText()).toEqual('not started');
+
+    button.click();
+    ptor.waitForAngular();
+
+    expect(status.getText()).toEqual('done');
+  });
+
+  it('DOES NOT wait for timeout', function() {
+    var status =
+      ptor.findElement(protractor.By.binding('slowTimeoutStatus'));
+    var button =
+      ptor.findElement(protractor.By.css('[ng-click="slowTimeout()"]'));
+
+    expect(status.getText()).toEqual('not started');
+
+    button.click();
+    ptor.waitForAngular();
+
+    expect(status.getText()).toEqual('pending...');
+  });
+
+  it('waits for $timeout', function() {
+    var status =
+      ptor.findElement(protractor.By.binding('slowAngularTimeoutStatus'));
+    var button =
+      ptor.findElement(protractor.By.css('[ng-click="slowAngularTimeout()"]'));
+
+    expect(status.getText()).toEqual('not started');
+
+    button.click();
+    ptor.waitForAngular();
+
+    expect(status.getText()).toEqual('done');
+  });
+
+  it('waits for $timeout then a promise', function() {
+    var status =
+      ptor.findElement(protractor.By.binding(
+          'slowAngularTimeoutPromiseStatus'));
+    var button =
+      ptor.findElement(protractor.By.css(
+          '[ng-click="slowAngularTimeoutPromise()"]'));
+
+    expect(status.getText()).toEqual('not started');
+
+    button.click();
+    ptor.waitForAngular();
+
+    expect(status.getText()).toEqual('done');
+  });
+
+  it('waits for long http call then a promise', function() {
+    var status =
+      ptor.findElement(protractor.By.binding('slowHttpPromiseStatus'));
+    var button =
+      ptor.findElement(protractor.By.css('[ng-click="slowHttpPromise()"]'));
+
+    expect(status.getText()).toEqual('not started');
+
+    button.click();
+    ptor.waitForAngular();
+
+    expect(status.getText()).toEqual('done');
+  });
 });
