e37c99cbb8d2e2706d3bf948cd1b9449ce58ff2d
Julie <ju.ralph@gmail.com>
2013-07-22 14:38:49 -0700

    Adding tests for the various types of slowness that might result in the view getting out of sync.

--- testapp/app/js/app.js
+++ testapp/app/js/app.js
@@ -8,5 +8,6 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives'])
     $routeProvider.when('/repeater', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
     $routeProvider.when('/bindings', {templateUrl: 'partials/bindings.html', controller: BindingsCtrl});
     $routeProvider.when('/form', {templateUrl: 'partials/form.html', controller: FormCtrl});
+    $routeProvider.when('/async', {templateUrl: 'partials/async.html', controller: AsyncCtrl});
     $routeProvider.otherwise({redirectTo: '/http'});
   }]);
