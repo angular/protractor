adb7e97309a6b709ee2ab6541453e9620fd45818
Julie <ju.ralph@gmail.com>
2013-08-28 12:42:16 -0700

    Improving the documentation in conf.js.

--- conf.js
+++ conf.js
@@ -1,11 +1,13 @@
-// An example configuration file.
+// A reference configuration file.
 exports.config = {
-  // ----- How to setup Selenium
+  // ----- How to setup Selenium -----
+  //
   // There are three ways to specify how to use Selenium. Specify one of the
   // following:
+  //
   // 1. seleniumServerJar - to start Selenium Standalone locally.
   // 2. seleniumAddress - to connect to a Selenium server which is already
-  //     running.
+  //    running.
   // 3. sauceUser/sauceKey - to use remote Selenium servers via SauceLabs.
 
   // The location of the selenium standalone server .jar file.
@@ -19,24 +21,28 @@ exports.config = {
   // attempt to find chromedriver using PATH.
   chromeDriver: './selenium/chromedriver',
   // Additional command line options to pass to selenium. For example,
-  // if  you need to change the browser timeout, use
-  // seleniumArgs: [-browserTimeout=60],
+  // if you need to change the browser timeout, use
+  // seleniumArgs: ['-browserTimeout=60'],
   seleniumArgs: [],
 
   // If sauceUser and sauceKey are specified, seleniumServerJar will be ignored.
+  // The tests will be run remotely using SauceLabs.
   sauceUser: null,
   sauceKey: null,
 
   // The address of a running selenium server.
   seleniumAddress: 'http://localhost:4444/wd/hub',
 
+  // ----- What tests to run -----
+  //
   // Spec patterns are relative to the current working directly when
   // protractor is called.
   specs: [
     'spec/*_spec.js',
   ],
 
-  // ----- Capabilities to be passed to the webdriver instance.
+  // ----- Capabilities to be passed to the webdriver instance ----
+  //
   // For a full list of available capabilities, see
   // https://code.google.com/p/selenium/wiki/DesiredCapabilities
   // and
@@ -49,12 +55,17 @@ exports.config = {
   // with relative paths will be prepended with this.
   baseUrl: 'http://localhost:8000',
 
-  // ----- Options to be passed to minijasminenode.
+  // ----- Options to be passed to minijasminenode -----
   jasmineNodeOpts: {
-    // onComplete will be called before the driver quits.
+    // onComplete will be called just before the driver quits.
     onComplete: null,
+    // If true, display spec names.
     isVerbose: false,
+    // If true, print colors to the terminal.
     showColors: true,
-    includeStackTrace: true
+    // If true, include stack traces in failures.
+    includeStackTrace: true,
+    // Default time to wait in ms before a test fails.
+    defaultTimeoutInterval: 5000
   }
 };
