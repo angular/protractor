3c76246a01e584bc30da645a36e75920b5397251
Colin Mutter <cmutter@redventures.com>
2013-08-27 20:54:31 -0400

    Added nested angular app capability via conf file.  Copied some tests over to demonstrate
    Changed option name to coincide with angular's $rootElement semantics

---
+++ spec/altRootConf.js
@@ -0,0 +1,29 @@
+// Tests for an Angular app where ng-app is not on the body.
+exports.config = {
+  seleniumServerJar: './selenium/selenium-server-standalone-2.35.0.jar',
+  chromeDriver: './selenium/chromedriver',
+
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // Spec patterns are relative to the current working directly when
+  // protractor is called.
+  specs: [
+    'spec/altRoot/*_spec.js',
+  ],
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // Selector for the element housing the angular app.
+  rootElement: 'div#nested-ng-app',
+
+  baseUrl: 'http://localhost:8000',
+
+  jasmineNodeOpts: {
+    onComplete: null,
+    isVerbose: false,
+    showColors: true,
+    includeStackTrace: true,
+  }
+};
