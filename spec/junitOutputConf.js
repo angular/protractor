d2633f10dd8d3abaa28a7450ca373b02f15e5392
Julie <ju.ralph@gmail.com>
2013-09-24 10:53:51 -0700

    docs(configuration): add an example configuration with xml output
    Adds an example configuration file showing how to use onPrepare to set up
    an additional Jasmine reporter.

---
+++ spec/junitOutputConf.js
@@ -0,0 +1,27 @@
+// This configuration file shows an example of using an Jasmine reporter to
+// output test results in XML format. Before running, you will need to
+// npm install jasmine-reporters
+require('jasmine-reporters');
+
+// The main suite of Protractor tests.
+exports.config = {
+  seleniumServerJar: './selenium/selenium-server-standalone-2.35.0.jar',
+  chromeDriver: './selenium/chromedriver',
+
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  specs: [
+    '*_spec.js'
+  ],
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  onPrepare: function() {
+    jasmine.getEnv().addReporter(
+      new jasmine.JUnitXmlReporter('xmloutput', true, true));
+  },
+
+  baseUrl: 'http://localhost:8000',
+};
