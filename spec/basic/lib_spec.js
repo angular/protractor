a2cd6c8baf242a81c4efea1f55249d597de95329
Julie <ju.ralph@gmail.com>
2013-10-26 23:02:19 -0700

    feat(syntax): big syntax reboot, expose global $, $$, element, and by
    In an effort to make tests more readable and clear, a few more global variables
    will now be exported.
    
    `browser` is an instance of protractor. This was previously accessed using
    `protractor.getInstance`.
    
    `by` is a collection of element locators. Previously, this was `protractor.By`.
    
    `$` is a shortcut for getting elements by css. `$('.foo')` === `element(by.css('.foo'))`
    
    All changes should be backwards compatible, as tested with the new 'backwardscompat'
    tests.
    Closes #156.

---
+++ spec/basic/lib_spec.js
@@ -0,0 +1,49 @@
+var util = require('util');
+
+describe('no ptor at all', function() {
+  it('should still do normal tests', function() {
+    expect(true).toBe(true);
+  });
+});
+
+describe('protractor library', function() {
+  it('should expose the correct global variables', function() {
+    expect(protractor).toBeDefined();
+    expect(browser).toBeDefined();
+    expect(by).toBeDefined();
+    expect(element).toBeDefined();
+    expect($).toBeDefined();
+  });
+
+  it('should wrap webdriver', function() {
+    browser.get('app/index.html');
+    expect(browser.getTitle()).toEqual('My AngularJS App');
+  });
+
+  it('should export custom parameters to the protractor instance', function() {
+    expect(browser.params.login).toBeDefined();
+    expect(browser.params.login.user).toEqual('Jane');
+    expect(browser.params.login.password).toEqual('1234');
+  });
+
+  it('should allow a mix of using protractor and using the driver directly',
+    function() {
+      browser.get('app/index.html');
+      expect(browser.getCurrentUrl()).
+          toEqual('http://localhost:8000/app/index.html#/http')
+
+      browser.driver.findElement(protractor.By.linkText('repeater')).click();
+      expect(browser.driver.getCurrentUrl()).
+          toEqual('http://localhost:8000/app/index.html#/repeater');
+
+      browser.navigate().back();
+      expect(browser.driver.getCurrentUrl()).
+          toEqual('http://localhost:8000/app/index.html#/http');
+    });
+
+  it('should export other webdriver classes onto the global protractor',
+      function() {
+        expect(protractor.ActionSequence).toBeDefined();
+        expect(protractor.Key.RETURN).toEqual('\uE006');
+    });
+});
