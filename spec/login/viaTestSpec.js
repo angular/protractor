267d49d8f38b867681c096fd1af5a6892ef7a5c0
Julie <ju.ralph@gmail.com>
2013-10-26 23:21:34 -0700

    chore(syntax): update all other specs to the new syntax

--- spec/login/viaTestSpec.js
+++ spec/login/viaTestSpec.js
@@ -1,39 +1,33 @@
 describe('pages with login', function() {
-  var ptor;
-
-  beforeEach(function() {
-    ptor = protractor.getInstance();
-  })
-
   it('should log in with a non-Angular page', function() {
-    ptor.driver.get('http://localhost:8000/app/login.html');
+    browser.driver.get('http://localhost:8000/app/login.html');
 
-    ptor.driver.findElement(protractor.By.id('username')).sendKeys('Jane');
-    ptor.driver.findElement(protractor.By.id('password')).sendKeys('1234');
-    ptor.driver.findElement(protractor.By.id('clickme')).click();
+    browser.driver.findElement(by.id('username')).sendKeys('Jane');
+    browser.driver.findElement(by.id('password')).sendKeys('1234');
+    browser.driver.findElement(by.id('clickme')).click();
 
     // Login takes some time, so wait until it's done.
     // For the test app's login, we know it's done when it redirects to
     // index.html.
-    ptor.wait(function() {
-      return ptor.driver.getCurrentUrl().then(function(url) {
+    browser.wait(function() {
+      return browser.driver.getCurrentUrl().then(function(url) {
         return /index/.test(url);
       });
     });
 
     // The login should have set a cookie. Make sure it's there.
-    ptor.manage().getCookie('testcookie').then(function(cookie) {
+    browser.manage().getCookie('testcookie').then(function(cookie) {
       expect(cookie.value).toEqual('Jane-1234');
     });
 
 
-    ptor.get('http://localhost:8000/app/index.html');
+    browser.get('http://localhost:8000/app/index.html');
 
-    var angularElement = ptor.findElement(protractor.By.input('url'));
+    var angularElement = element(by.input('url'));
     expect(angularElement.getAttribute('value')).toEqual('/fastcall');
 
     // Make sure the cookie is still set.
-    ptor.manage().getCookie('testcookie').then(function(cookie) {
+    browser.manage().getCookie('testcookie').then(function(cookie) {
       expect(cookie.value).toEqual('Jane-1234');
     });
   });
--- spec/onPrepare/startup.js
+++ spec/onPrepare/startup.js
@@ -1,5 +1,1 @@
-var ptor = protractor.getInstance();
-ptor.elem = ptor.findElement;
-ptor.elems = ptor.findElements;
-global.by = protractor.By;
-global.ptor = ptor;
+browser.params.password = '12345';
