a4ffef8c7efe581ce2f56db6902c21138178353c
Jeff Froom <jeff@jfroom.com>
2013-10-30 16:59:22 -0500

    chore(selenium): update to latest version
    - Old .jar and chromedriver versions out of date.
    - Chromedriver URL paths slightly changed as well.
    - Paths found on this page: http://www.seleniumhq.org/download/
    
    No known breaking changes.

--- referenceConf.js
+++ referenceConf.js
@@ -11,7 +11,7 @@ exports.config = {
   // 3. sauceUser/sauceKey - to use remote Selenium servers via SauceLabs.
 
   // The location of the selenium standalone server .jar file.
-  seleniumServerJar: './selenium/selenium-server-standalone-2.35.0.jar',
+  seleniumServerJar: './selenium/selenium-server-standalone-2.37.0.jar',
   // The port to start the selenium server on, or null if the server should
   // find its own unused port.
   seleniumPort: null,
