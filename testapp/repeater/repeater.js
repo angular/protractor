927ba2c2f847aa8fb0545cb7e58a7725f9057e4d
Julie <ju.ralph@gmail.com>
2013-10-30 15:31:20 -0700

    chore(testapp): cleaning up the test application
    Remove the little used 'http' page, which was a leftover from the
    angular http example.
    
    Repurpose the 'repeater' page to hold all the repeater info.
    
    Add a couple more tests for the global element variable, and remove
    the backwards compatability suite.

--- testapp/repeater/repeater.js
+++ testapp/repeater/repeater.js
@@ -1,23 +1,11 @@
 function RepeaterCtrl($scope) {
-  function fib(n) {
-    if (n <= 1) return 1;
-    return fib(n - 1) + fib(n - 2);
-  }
-
-  $scope.foos = [];
-  for (var i = 0; i < 35; i++) {
-    $scope.foos[i] = {
-      a: i,
-      b: fib(i),
-    };
-  }
-
-  $scope.addOne = function() {
-    $scope.foos.push({
-      a: $scope.foos.length,
-      b: fib($scope.foos.length)
-    });
-  };
-}
+  $scope.days = [
+    {initial: 'M', name: 'Monday'},
+    {initial: 'T', name: 'Tuesday'},
+    {initial: 'W', name: 'Wednesday'},
+    {initial: 'Th', name: 'Thursday'},
+    {initial: 'F', name: 'Friday'},
+  ];
+};
 
 RepeaterCtrl.$inject = ['$scope'];
