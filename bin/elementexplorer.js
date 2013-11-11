7df7d6b0882ab5f6a1cee82b0b0f86275dc25746
MexicanAce <nicolasvillanueva@msn.com>
2013-11-11 11:25:48 -0500

    Feature: Highlight WebElements by locator (Issue #241)
    This new feature would create a flashing border surrounding all visible WebElements found by a given locator. An example of how it can be used against http://angularjs.org/ is: `highlight(by.binding(''))`

--- bin/elementexplorer.js
+++ bin/elementexplorer.js
@@ -66,6 +66,106 @@ var list = function(locator) {
   });
 };
 
+var highlight = function(locator) {
+    var xPaths = [];
+    var cssBorders = [];
+
+    return browser.findElements(locator).then(function(arr) {
+        for (var i = 0; i < arr.length; ++i) {
+            getAbsoluteXPath(arr[i]).then(function(result){
+                xPaths.push(result);
+            });
+
+            arr[i].getCssValue("border").then(function(val) {
+                cssBorders.push(val);
+            });
+        }
+
+        if(arr.length == 0) {
+            return "No elements found.";
+        }
+    }).then(function() {
+        var timesFlashed = 10;
+        var waitTime = 500;
+
+        for (var t = 0; t < timesFlashed; t++) {
+            var borderValue = "5px inset rgb(256, 0, 0)";
+            if (t%2 == 1) {
+                borderValue = "5px inset rgb(256, 256, 0)";
+            }
+            for (var i = 0; i < xPaths.length; i++) {
+                driver.executeScript("var x = document.evaluate('" + xPaths[i] + "', document, null, XPathResult.ANY_TYPE, null); var xx = x.iterateNext(); xx.style.border = '" + borderValue + "';"); 
+            }
+
+            driver.sleep(waitTime);
+        }
+    }).then(function() {
+        for (var j = 0; j < cssBorders.length; j++) {
+            driver.executeScript("var x = document.evaluate('" + xPaths[j] + "', document, null, XPathResult.ANY_TYPE, null); var xx = x.iterateNext(); xx.style.border = '" + cssBorders[j] + "';");   
+        }
+
+        return "Highlighting Completed.";
+    });
+};
+
+var getAbsoluteXPath = function(element) {
+        return driver.executeScript(
+                "function absoluteXPath(element) {"+
+                        "var comp, comps = [];"+
+                        "var parent = null;"+
+                        "var xpath = '';"+
+                        "var getPos = function(element) {"+
+                        "var position = 1, curNode;"+
+                        "if (element.nodeType == Node.ATTRIBUTE_NODE) {"+
+                        "return null;"+
+                        "}"+
+                        "for (curNode = element.previousSibling; curNode; curNode = curNode.previousSibling) {"+
+                        "if (curNode.nodeName == element.nodeName) {"+
+                        "++position;"+
+                        "}"+
+                        "}"+
+                        "return position;"+
+                        "};"+
+
+    "if (element instanceof Document) {"+
+    "return '/';"+
+    "}"+
+
+    "for (; element && !(element instanceof Document); element = element.nodeType == Node.ATTRIBUTE_NODE ? element.ownerElement : element.parentNode) {"+
+    "comp = comps[comps.length] = {};"+
+    "switch (element.nodeType) {"+
+    "case Node.TEXT_NODE:"+
+    "comp.name = 'text()';"+
+    "break;"+
+    "case Node.ATTRIBUTE_NODE:"+
+    "comp.name = '@' + element.nodeName;"+
+    "break;"+
+    "case Node.PROCESSING_INSTRUCTION_NODE:"+
+    "comp.name = 'processing-instruction()';"+
+    "break;"+
+    "case Node.COMMENT_NODE:"+
+    "comp.name = 'comment()';"+
+    "break;"+
+    "case Node.ELEMENT_NODE:"+
+    "comp.name = element.nodeName;"+
+    "break;"+
+    "}"+
+    "comp.position = getPos(element);"+
+    "}"+
+
+    "for (var i = comps.length - 1; i >= 0; i--) {"+
+    "comp = comps[i];"+
+    "xpath += '/' + comp.name.toLowerCase();"+
+    "if (comp.position !== null) {"+
+    "xpath += '[' + comp.position + ']';"+
+    "}"+
+    "}"+
+
+    "return xpath;"+
+
+    "} return absoluteXPath(arguments[0]);", element);
+};
+
 var flowEval = function(code, context, file, callback) {
 
   var vmErr,
@@ -135,7 +235,7 @@ var startUp = function() {
     global.element = browser.element;
     global.by = protractor.By;
     global.list = list;
-
+    global.highlight = highlight;
 
     util.puts('Type <tab> to see a list of locator strategies.');
     util.puts('Use the `list` helper function to find elements by strategy:');
