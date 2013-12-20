b7ab3dae87ceae9d4e6441944a50338ccf2a3bbe
Julie <ju.ralph@gmail.com>
2013-12-19 18:43:36 -0800

    chore(config): remove references to old versions of selenium standalone

--- spec/onPrepareConf.js
+++ spec/onPrepareConf.js
@@ -1,8 +1,5 @@
 // The main suite of Protractor tests.
 exports.config = {
-  seleniumServerJar: '../selenium/selenium-server-standalone-2.35.0.jar',
-  chromeDriver: '../selenium/chromedriver',
-
   seleniumAddress: 'http://localhost:4444/wd/hub',
 
   // Spec patterns are relative to this directory.
--- spec/onPrepareStringConf.js
+++ spec/onPrepareStringConf.js
@@ -1,9 +1,6 @@
 // Configuration using a string in onPrepare to load a file with code to
 // execute once before tests.
 exports.config = {
-  seleniumServerJar: '../selenium/selenium-server-standalone-2.35.0.jar',
-  chromeDriver: '../selenium/chromedriver',
-
   seleniumAddress: 'http://localhost:4444/wd/hub',
 
   // Spec patterns are relative to this directory.
