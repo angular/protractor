f52438549f7d920da1600199feaf58059d6fd692
Julie <ju.ralph@gmail.com>
2013-12-27 16:38:54 -0800

    cleanup(specs): moving all configs into the same location and adding login to npm test specs

--- spec/login/login_spec.js
+++ spec/login/login_spec.js
@@ -2,8 +2,8 @@ describe('pages with login', function() {
   it('should log in with a non-Angular page', function() {
     browser.get('http://localhost:8000/index.html');
 
-    var angularElement = element(by.input('url'));
-    expect(angularElement.getAttribute('value')).toEqual('/fastcall');
+    var angularElement = element(by.model('username'));
+    expect(angularElement.getAttribute('value')).toEqual('Anon');
 
     // Make sure the cookie is still set.
     browser.manage().getCookie('testcookie').then(function(cookie) {
