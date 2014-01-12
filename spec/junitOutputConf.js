9c86a27c15cf2c9f60aae0c002b8fc98177bf3be
Julie <ju.ralph@gmail.com>
2014-01-12 11:46:27 -0800

    fix(config): note that the last release contained a breaking change on when jasmine is available
    Because frameworks are loaded on-demand, `jasmine` is not available until inside onPrepare.

--- spec/junitOutputConf.js
+++ spec/junitOutputConf.js
@@ -1,7 +1,6 @@
 // This configuration file shows an example of using an Jasmine reporter to
 // output test results in XML format. Before running, you will need to
 // npm install jasmine-reporters
-require('jasmine-reporters');
 
 // The main suite of Protractor tests.
 exports.config = {
@@ -16,8 +15,19 @@ exports.config = {
   },
 
   onPrepare: function() {
+    // The require statement must be down here, since jasmine-reporters
+    // needs jasmine to be in the global and protractor does not guarantee
+    // this until inside the onPrepare function.
+    require('jasmine-reporters');
     jasmine.getEnv().addReporter(
       new jasmine.JUnitXmlReporter('xmloutput', true, true));
+  },
+
+  params: {
+    login: {
+      user: 'Jane',
+      password: '1234'
+    }
   },
 
   baseUrl: 'http://localhost:8000',
