7bb66c6cd849cb6114f063eb1190dc1198e503df
Michael Vera <michael.vera@points.com>
2014-01-29 10:39:43 -0500

    feat(config): add option to exclude specs based on file patterns

---
+++ spec/excludesConf.js
@@ -0,0 +1,27 @@
+// The main suite of Protractor tests.
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // Spec patterns are relative to this directory.
+  specs: [
+    'basic/*_spec.js'
+  ],
+
+  // Exclude patterns are relative to this directory.
+  excludes: [
+    'basic/*_exclude.js'
+  ],
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  baseUrl: 'http://localhost:8000',
+
+  params: {
+    login: {
+      user: 'Jane',
+      password: '1234'
+    }
+  }
+};
