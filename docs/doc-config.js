2473d507fc226319aa977dd1e34f34bc7bbe126a
andres <code@karateca.com>
2014-02-15 14:35:28 -0500

    remove noise

--- docs/doc-config.js
+++ docs/doc-config.js
@@ -8,10 +8,9 @@ module.exports = function (config) {
 
   config = basePackage(config);
 
-  var processors =  config.processing.processors.concat([
+  var processors = config.processing.processors.concat([
     { name: 'do-stuff', runAfter: ['processing-docs'] }
   ]);
-
 
   config.append('processing.processors', [
     {
@@ -30,48 +29,22 @@ module.exports = function (config) {
     }
   ]);
 
-
   var basePath = path.resolve(packagePath);
-
   basePath = '/Users/andresdom/dev/protractor';
 
   config.set('source.files', [
     { pattern: 'lib/**/*.js', basePath: basePath }
-//    ,
-//    { pattern: '**/*.ngdoc', basePath: path.resolve(packagePath, 'content') }
   ]);
 
-//  config.set('source.currentVersion', currentVersion);
-//  config.set('source.previousVersions', previousVersions);
-
   config.set('rendering.outputFolder', 'build');
-
   config.set('logging.level', 'debug');
-
 
   var docsPath = path.resolve(basePath, 'docs');
   config.set('rendering.templateFolders', [docsPath]);
 
   config.set('rendering.templatePatterns', [
     'api-template.md'
-//    ,
-//    '${ doc.template }',
-//    '${doc.area}/${ doc.id }.${ doc.docType }.template.html',
-//    '${doc.area}/${ doc.id }.template.html',
-//    '${doc.area}/${ doc.docType }.template.html',
-//    '${ doc.id }.${ doc.docType }.template.html',
-//    '${ doc.id }.template.html',
-//    '${ doc.docType }.template.html'
   ]);
-//  config.merge('rendering.extra', {
-//    git: { owner: 'angular', repo: 'angular.js' },
-//    version: currentVersion
-//  });
-
-//  config.set('processing.examples.commonFiles', {
-//    scripts: [ '../../angular.js' ],
-//    stylesheets: []
-//  });
 
   return config;
 };
