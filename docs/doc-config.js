64686ad0a211d4015395d9e7d5cd6527b1fa3b78
andres <code@karateca.com>
2014-02-15 14:34:07 -0500

    add a custom processor

--- docs/doc-config.js
+++ docs/doc-config.js
@@ -8,6 +8,29 @@ module.exports = function (config) {
 
   config = basePackage(config);
 
+  var processors =  config.processing.processors.concat([
+    { name: 'do-stuff', runAfter: ['processing-docs'] }
+  ]);
+
+
+  config.append('processing.processors', [
+    {
+      name: 'do-stuff',
+      description: 'Doing some stuff',
+      runAfter: ['parsing-tags'],
+      runBefore: ['tags-parsed'],
+//      runAfter: ['rendering-docs'],
+//      runBefore: ['docs-rendered'],
+      init: function (config) {
+//        console.log(config);
+      },
+      process: function (docs) {
+        console.log(docs);
+      }
+    }
+  ]);
+
+
   var basePath = path.resolve(packagePath);
 
   basePath = '/Users/andresdom/dev/protractor';
@@ -25,6 +48,21 @@ module.exports = function (config) {
 
   config.set('logging.level', 'debug');
 
+
+  var docsPath = path.resolve(basePath, 'docs');
+  config.set('rendering.templateFolders', [docsPath]);
+
+  config.set('rendering.templatePatterns', [
+    'api-template.md'
+//    ,
+//    '${ doc.template }',
+//    '${doc.area}/${ doc.id }.${ doc.docType }.template.html',
+//    '${doc.area}/${ doc.id }.template.html',
+//    '${doc.area}/${ doc.docType }.template.html',
+//    '${ doc.id }.${ doc.docType }.template.html',
+//    '${ doc.id }.template.html',
+//    '${ doc.docType }.template.html'
+  ]);
 //  config.merge('rendering.extra', {
 //    git: { owner: 'angular', repo: 'angular.js' },
 //    version: currentVersion
