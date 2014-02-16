70f79b39516b1d1b297c29793b949d17cda7d726
andres <code@karateca.com>
2014-02-16 01:45:02 -0500

    new filter

---
+++ docs/tag-fixer.js
@@ -0,0 +1,25 @@
+var removeNewLines = function(doc) {
+  if (doc.tags) {
+    doc.tags.tags.forEach(function (tag) {
+      tag.description = (tag.description || '').replace('\n', ' ');
+    })
+  }
+};
+
+module.exports = {
+  name: 'do-stuff',
+  description: 'Doing some stuff',
+  runAfter: ['extracting-tags'],
+  init: function (config) {
+  },
+  process: function (docs) {
+    var i = 1;
+    docs.forEach(function(doc) {
+      removeNewLines(doc);
+
+      // Get everything until the first @.
+      doc.desc = /[^@]*/.exec(doc.content || '')[0];
+      doc.outputPath = 'partials/' + doc.fileName + (i++) + '.md'
+    });
+  }
+}
