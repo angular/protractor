3fb8ddc39b05cab1a319532233b0ed81aca6b663
andres <code@karateca.com>
2014-02-16 01:51:47 -0500

    new renderer

---
+++ docs/doc-renderer.js
@@ -0,0 +1,21 @@
+var nunjucks = require('nunjucks');
+var fs = require('fs');
+
+var templateFile;
+
+module.exports = {
+  name: 'proper-render',
+  description: 'Do the rendering',
+  runAfter: ['rendering-docs'],
+  runBefore: ['docs-rendered'],
+  init: function () {
+    templateFile = fs.readFileSync('/Users/andresdom/dev/protractor/docs/api-template.md', 'utf-8');
+  },
+  process: function (docs) {
+    docs.forEach(function (doc) {
+      console.log(doc);
+      nunjucks.configure({autoescape: true});
+      doc.renderedContent = nunjucks.renderString(templateFile, doc);
+    });
+  }
+};
