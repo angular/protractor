46f8ea24101154d5a060571f639dc7244b425a6c
andres <code@karateca.com>
2014-02-16 19:35:45 -0500

    formatting

--- docs/spec/tag-fixer-spec.js
+++ docs/spec/tag-fixer-spec.js
@@ -1,17 +1,21 @@
-describe('tag fixer', function () {
-  var doc = {
-    file: 'lib/protractor.js',
-    content: 'element.all is used for operations on an array of elements (as opposed\nto a single element).\n\nExample:\n    var lis = element.all(by.css(\'li\'));\n    browser.get(\'myurl\');\n    expect(lis.count()).toEqual(4);\n\n@name element.all\n@param {webdriver.Locator} locator\n@return {ElementArrayFinder}',
-    fileName: 'protractor',
-    name: 'element.all',
-    description: 'element.all is used for operations on an array of elements (as opposed to a single element).\n\nExample:\n    var lis = element.all(by.css(\'li\'));\n    browser.get(\'myurl\');\n    expect(lis.count()).toEqual(4);'
-  };
+var tagFixer = require('../tag-fixer');
 
-  beforeEach(function () {
+describe('tag fixer', function() {
+  var docs;
 
+  beforeEach(function() {
+    docs = [
+      {
+        file: 'lib/protractor.js',
+        content: 'element.all is used for operations on an array of elements (as opposed\nto a single element).\n\nExample:\n    var lis = element.all(by.css(\'li\'));\n    browser.get(\'myurl\');\n    expect(lis.count()).toEqual(4);\n\n@name element.all\n@param {webdriver.Locator} locator\n@return {ElementArrayFinder}',
+        fileName: 'protractor',
+        name: 'element.all',
+        description: 'element.all is used for operations on an array of elements (as opposed to a single element).\n\nExample:\n    var lis = element.all(by.css(\'li\'));\n    browser.get(\'myurl\');\n    expect(lis.count()).toEqual(4);'
+      }
+    ]
   });
 
-  it('should parse example', function () {
+  it('should parse example', function() {
 
   });
 });
