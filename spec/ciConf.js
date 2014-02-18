fcf65a676c7af5fbf705b2cd1818ab4b6e702d65
Julie <ju.ralph@gmail.com>
2014-02-18 09:50:14 -0800

    chore(release): fix Travis configuration so that sauce tunnel is properly passed

---
+++ spec/ciConf.js
@@ -0,0 +1,36 @@
+// The main suite of Protractor tests to be run on CI servers.
+exports.config = {
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+
+  // Spec patterns are relative to this directory.
+  specs: [
+    'basic/*_spec.js'
+  ],
+
+  // Exclude patterns are relative to this directory.
+  exclude: [
+    'basic/exclude*.js'
+  ],
+
+  multiCapabilities: [{
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'Protractor smoke tests'
+  }, {
+    'browserName': 'firefox',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'Protractor smoke tests'
+  }],
+
+  baseUrl: 'http://localhost:8000',
+
+  params: {
+    login: {
+      user: 'Jane',
+      password: '1234'
+    }
+  }
+};
