49f9ad6bd86ffa8ab0055c684c94dae2cac08e7d
andres <code@karateca.com>
2014-02-21 21:53:46 -0500

    lint

--- docs/processors/js.js
+++ docs/processors/js.js
@@ -22,12 +22,14 @@ module.exports = {
   processFile: function(filePath, contents, basePath) {
 
     // Bail out if there is no contents
-    if ( !contents ) { return []; }
+    if (!contents) {
+      return [];
+    }
 
     var docs = [], currentDoc, match,
         lines = contents.split(NEW_LINE);
 
-    lines.forEach(function(line, lineNumber){
+    lines.forEach(function(line, lineNumber) {
 
       // is the comment starting?
       if (!currentDoc && (match = line.match(DOC_COMMENT_START))) {
@@ -38,7 +40,7 @@ module.exports = {
         // Create a new doc, merging in any default properties
         currentDoc = {
           fileType: 'js',
-          startingLine: lineNumber+1, // We want lines to be 1-based
+          startingLine: lineNumber + 1, // We want lines to be 1-based
           file: filePath,
           basePath: basePath,
           content: ''
@@ -66,7 +68,7 @@ module.exports = {
       }
 
       // we are in a doc comment so add text (removing leading stars)
-      if (currentDoc){
+      if (currentDoc) {
         currentDoc.content += '\n' + line.replace(LEADING_STAR, '');
       }
     });
