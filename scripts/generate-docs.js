325477818da8b41a1d8b517f51ab63d217310c80
andres <code@karateca.com>
2014-02-23 22:13:49 -0500

    Add some comments

--- scripts/generate-docs.js
+++ scripts/generate-docs.js
@@ -39,9 +39,13 @@ function generateDocs() {
         return fileName.replace(/\d*\.md/, '');
       });
 
+      // Sort the files in the following order:
+      // toc - table of contents
+      // protractor
+      // locators
+      // webdriver
       var fileOrder = ['toc', 'protractor', 'locators', 'webdriver'];
       fileOrder.forEach(function(fileName) {
-        console.log(fileName);
         sortedFiles = sortedFiles.concat(grouped[fileName]);
       });
 
@@ -50,12 +54,16 @@ function generateDocs() {
   });
 }
 
+/**
+ * Merge all the partial files into the final api.md file.
+ * @param {Array.<string>} files Partial files containing the functions.
+ */
 function mergeFiles(files) {
   console.log('Merging ' + files.length + ' files into ' + apiFileName);
 
   var buffer = [];
 
-  // Read all the files.
+  // Read each file and put them into the buffer.
   files.forEach(function(file) {
     var contents = fs.readFileSync(path.resolve(buildPath, file), 'utf-8');
     buffer.push(contents);
@@ -65,14 +73,14 @@ function mergeFiles(files) {
     return;
   }
 
-  // Write file.
-  var joinedPath = path.resolve(docsPath, 'api.md'),
+  // Write the buffer into the output file.
+  var apiFilePath = path.resolve(docsPath, apiFileName),
       contents = buffer.join('\n');
 
-  fs.writeFile(joinedPath, contents, function(err) {
+  fs.writeFile(apiFilePath, contents, function(err) {
     if (err) {
       throw err;
     }
-    console.log('Done writing file');
+    console.log('Done writing file', apiFilePath);
   });
 }
