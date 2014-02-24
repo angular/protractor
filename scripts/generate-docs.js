7fdf9870e55239391e0eb70bf1a53d9901e66009
andres <code@karateca.com>
2014-02-23 20:48:41 -0500

    Delete build dir, generate.

--- scripts/generate-docs.js
+++ scripts/generate-docs.js
@@ -1,12 +1,25 @@
 var docGenerator = require('dgeni');
+var rimraf = require('rimraf');
 var path = require('canonical-path');
 
 (function() {
   var configPath = path.resolve(__dirname, '../docs/doc-config.js');
 
-  docGenerator(configPath).generateDocs().then(function(docs) {
-    console.log("I am done");
+  var buildPath = path.resolve(__dirname, '../docs/build');
+
+  console.log('Deleting build directory', buildPath);
+  rimraf(buildPath, function(err) {
+    if (err) {
+      throw  err;
+    }
+    console.log('Done deleting build directory');
+    generateDocs();
   });
 
-
+  function generateDocs() {
+    console.log('Generating docs');
+    docGenerator(configPath).generateDocs().then(function(docs) {
+      console.log("I am done");
+    });
+  }
 })();
