560473ceaf5862d73bb5f5a2c7ec10ac1b93678a
andres <code@karateca.com>
2014-02-23 22:06:46 -0500

    remove wrapping fn

--- scripts/generate-docs.js
+++ scripts/generate-docs.js
@@ -4,77 +4,75 @@ var fs = require('fs');
 var path = require('canonical-path');
 var _ = require('lodash');
 
-//(function() {
-  var apiFileName = 'api.md',
-      configPath = path.resolve(__dirname, '../docs/doc-config.js'),
-      buildPath = path.resolve(__dirname, '../docs/build'),
-      docsPath = path.resolve(__dirname, '../docs');
+var apiFileName = 'api.md',
+    configPath = path.resolve(__dirname, '../docs/doc-config.js'),
+    buildPath = path.resolve(__dirname, '../docs/build'),
+    docsPath = path.resolve(__dirname, '../docs');
 
-  deleteBuild(function() {
-    generateDocs();
+deleteBuildDir(function() {
+  generateDocs();
+});
+
+function deleteBuildDir(doneCallback) {
+  console.log('Deleting build directory', buildPath);
+  rimraf(buildPath, function(err) {
+    if (err) {
+      throw  err;
+    }
+    console.log('Done deleting build directory');
+    doneCallback();
   });
+}
 
-  function deleteBuild(doneCallback) {
-    console.log('Deleting build directory', buildPath);
-    rimraf(buildPath, function(err) {
+function generateDocs() {
+  console.log('Generating docs');
+  docGenerator(configPath).generateDocs().then(function() {
+    fs.readdir(buildPath, function(err, files) {
       if (err) {
         throw  err;
       }
-      console.log('Done deleting build directory');
-      doneCallback();
-    });
-  }
 
-  function generateDocs() {
-    console.log('Generating docs');
-    docGenerator(configPath).generateDocs().then(function() {
-      fs.readdir(buildPath, function(err, files) {
-        if (err) {
-          throw  err;
-        }
+      var sortedFiles = [];
 
-        var sortedFiles = [];
-
-        // Group by file name.
-        var grouped = _.groupBy(files, function(fileName) {
-          return fileName.replace(/\d*\.md/, '');
-        });
-
-        var fileOrder = ['toc', 'protractor', 'locators', 'webdriver'];
-        fileOrder.forEach(function(fileName) {
-          console.log(fileName);
-          sortedFiles = sortedFiles.concat(grouped[fileName]);
-        });
-
-        mergeFiles(sortedFiles);
+      // Group by file name.
+      var grouped = _.groupBy(files, function(fileName) {
+        return fileName.replace(/\d*\.md/, '');
       });
+
+      var fileOrder = ['toc', 'protractor', 'locators', 'webdriver'];
+      fileOrder.forEach(function(fileName) {
+        console.log(fileName);
+        sortedFiles = sortedFiles.concat(grouped[fileName]);
+      });
+
+      mergeFiles(sortedFiles);
     });
+  });
+}
+
+function mergeFiles(files) {
+  console.log('Merging ' + files.length + ' files into ' + apiFileName);
+
+  var buffer = [];
+
+  // Read all the files.
+  files.forEach(function(file) {
+    var contents = fs.readFileSync(path.resolve(buildPath, file), 'utf-8');
+    buffer.push(contents);
+  });
+
+  if (buffer.length === 0) {
+    return;
   }
 
-  function mergeFiles(files) {
-    console.log('Merging ' + files.length + ' files into ' + apiFileName);
+  // Write file.
+  var joinedPath = path.resolve(docsPath, 'api.md'),
+      contents = buffer.join('\n');
 
-    var buffer = [];
-
-    // Read all the files.
-    files.forEach(function(file) {
-      var contents = fs.readFileSync(path.resolve(buildPath, file), 'utf-8');
-      buffer.push(contents);
-    });
-
-    if (buffer.length === 0) {
-      return;
+  fs.writeFile(joinedPath, contents, function(err) {
+    if (err) {
+      throw err;
     }
-
-    // Write file.
-    var joinedPath = path.resolve(docsPath, 'api.md'),
-        contents = buffer.join('\n');
-
-    fs.writeFile(joinedPath, contents, function(err) {
-      if (err) {
-        throw err;
-      }
-      console.log('Done writing file');
-    });
-  }
-//})();
+    console.log('Done writing file');
+  });
+}
