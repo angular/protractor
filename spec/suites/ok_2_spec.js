eabe0f0832086f7a273a0372af530f0822e1e864
Sebastien Armand - sa250111 <sebastien.armand@crm-factory.com.cn>
2014-02-24 11:35:37 +0800

    feat(cli+config)
    Allow defining multiple test suites in the config and running them separately. Add testing for new config option.

---
+++ spec/suites/ok_2_spec.js
@@ -0,0 +1,5 @@
+describe('This test suite', function(){
+    it('should be ran through the --suite option', function(){
+        expect(true).toBe(true);
+    });
+});
---
+++ spec/suitesConf.js
@@ -0,0 +1,31 @@
+// The main suite of Protractor tests.
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // Spec patterns are relative to this directory.
+  specs: {
+    okspec: 'suites/ok_spec.js',
+    okmany: ['suites/ok_spec.js', 'suites/ok_2_spec.js'],
+    failingtest: 'suites/always_fail_spec.js'
+  },
+
+  // Exclude patterns are relative to this directory.
+  exclude: [
+    'basic/exclude*.js'
+  ],
+
+  chromeOnly: false,
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  baseUrl: 'http://localhost:8000',
+
+  params: {
+    login: {
+      user: 'Jane',
+      password: '1234'
+    }
+  }
+};
