df6e2b81aa33c824ba31877456796a7a203ee5ae
andres <code@karateca.com>
2014-02-24 22:56:17 -0500

    Generate the file name

--- docs/processors/filter-jsdoc.js
+++ docs/processors/filter-jsdoc.js
@@ -27,14 +27,42 @@ var filterDocs = function(docs) {
   });
 };
 
+/**
+ * Generate a unique file name with an index used to concatenate. The name has
+ * the following format: 'protractor001.md'
+ */
+var fileName = function(doc) {
+  counter += 1;
+
+  // Generate a counter of 4 characters.
+  var index = '000' + counter;
+  index = index.substring(index.length - 4);
+  return doc.fileName + index + '.md';
+};
+
+/**
+ * Used to generate a sequence number for each function.
+ * @type {number}
+ */
+var counter = 1;
+
 module.exports = {
   name: 'filter-jsdoc',
-  description: 'Filter functions that will not go into the documentation',
+  description: 'Filter functions that will not go into the documentation and ' +
+      'generate a name for the output partial file that can be sorted ' +
+      'alphabetically',
   runAfter: ['extracting-tags'],
   runBefore: ['tags-extracted'],
   init: function(config) {
   },
   process: function(docs) {
-    return filterDocs(docs);
+    docs = filterDocs(docs);
+
+    // Generate the output file name.
+    docs.forEach(function(doc) {
+      doc.outputPath = fileName(doc);
+    });
+
+    return  docs;
   }
 };
