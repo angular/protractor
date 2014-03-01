291a5edf28678d17e1e1340b258dda154250a4c5
andres <code@karateca.com>
2014-02-28 23:29:07 -0500

    Add test for type link parsing

--- docs/spec/add-links-spec.js
+++ docs/spec/add-links-spec.js
@@ -3,8 +3,8 @@ var expect = require('expect.js');
 
 
 describe('add-links', function() {
-  var addLinks = function(doc) {
-    linksProcessor.process([doc]);
+  var addLinks = function(docs) {
+    linksProcessor.process(docs);
   };
 
   it('should add protractor link', function() {
@@ -12,7 +12,7 @@ describe('add-links', function() {
       fileName: 'protractor',
       startingLine: 123
     };
-    addLinks(doc);
+    addLinks([doc]);
     expect(doc.sourceLink).to.equal('https://github.com/angular/protractor/' +
         'blob/master/lib/protractor.js#L123');
   });
@@ -22,8 +22,38 @@ describe('add-links', function() {
       fileName: 'webdriver',
       startingLine: 123
     };
-    addLinks(doc);
+    addLinks([doc]);
     expect(doc.sourceLink).to.equal('https://code.google.com/p/selenium/' +
         'source/browse/javascript/webdriver/webdriver.js#123');
+  });
+
+  it('should add links to types', function() {
+    // Given a type and a function.
+    var docs = [
+      {
+        name: 'webdriver.WebElement',
+        fileName: 'webdriver',
+        startingLine: 123
+      },
+      {
+        name: 'theFunction',
+        fileName: 'protractor',
+        startingLine: 123,
+        params: [
+          {
+            type: {
+              description: '!webdriver.WebElement'
+            }
+          }
+        ]
+      }
+    ];
+
+    // When you add links.
+    addLinks(docs);
+
+    // Then ensure the link was added.
+    expect(docs[1].params[0].type.description).
+        to.equal('[!webdriver.WebElement](#webdriverwebelement)');
   });
 });
