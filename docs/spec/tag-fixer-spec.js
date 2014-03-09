20ad02ee76999354f2f50ec23618027383399bf3
andres <code@karateca.com>
2014-03-08 22:24:57 -0500

    docs(tag-fixer) Update the test to reflect the new document structure.

--- docs/spec/tag-fixer-spec.js
+++ docs/spec/tag-fixer-spec.js
@@ -1,28 +1,75 @@
 var tagFixer = require('../processors/tag-fixer');
 
 describe('tag fixer', function() {
-  var doc;
+  var expressionDoc, declarationDoc, docs;
 
   beforeEach(function() {
-    doc = {
-      name: 'element.all',
-      fnDef: {line: '  var element = function(locator) {', lineNumber: 165},
+    expressionDoc = {
+      code: {
+        node: {
+          expression: {
+            left: {
+              object: {
+                name: 'element'
+              },
+              property: {
+                name: 'all'
+              }
+            }
+          }
+        }
+      },
       tags: {
-        description: 'element'
+        description: 'element description'
       }
     };
+
+    declarationDoc = {
+      code: {
+        node: {
+          declarations: [
+            {
+              id: {
+                name: 'element'
+              }
+            }
+          ]
+        }
+      },
+      tags: {
+        description: 'element description'
+      }
+    };
+
+    docs = [expressionDoc, declarationDoc];
   });
 
-  it('should find name in fnDef', function() {
-    // Remove the name.
-    doc.name = null;
-    tagFixer.process([doc]);
+  it('should find name in code expression', function() {
+    // When you process the docs.
+    tagFixer.process(docs);
 
-    expect(doc.name).toBe('element');
+    // Then ensure the name was parsed.
+    expect(expressionDoc.name).toBe('element.all');
+  });
+
+  it('should find name in code declaration', function() {
+    // When you process the docs.
+    tagFixer.process(docs);
+
+    // Then ensure the name was parsed.
+    expect(declarationDoc.name).toBe('element');
   });
 
   it('should not override name', function() {
-    tagFixer.process([doc]);
-    expect(doc.name).toBe('element.all');
+    // Given that the doc has a @name.
+    expressionDoc.name = 'name1';
+    declarationDoc.name = 'name2';
+
+    // When you process the docs.
+    tagFixer.process(docs);
+
+    // Then ensure the name was not changed.
+    expect(expressionDoc.name).toBe('name1');
+    expect(declarationDoc.name).toBe('name2');
   });
 });
