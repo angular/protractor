753974f59b38161c6a55597b2e329e4c500a35c6
Parashuram <code@r.nparashuram.com>
2014-03-23 22:27:55 -0700

    (fix): Convert test.sh to test.js
    This would enable the tests to be run on both Linux and Windows.

---
+++ scripts/test.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+var glob = require('glob').sync;
+var spawn = require('child_process').spawn;
+
+var scripts = [
+  'lib/cli.js spec/basicConf.js',
+  'lib/cli.js spec/multiConf.js',
+  'lib/cli.js spec/altRootConf.js',
+  'lib/cli.js spec/onPrepareConf.js',
+  'lib/cli.js spec/mochaConf.js',
+  'lib/cli.js spec/cucumberConf.js',
+  'node lib/cli.js spec/withLoginConf.js',
+  'node lib/cli.js spec/suitesConf.js --suite okmany',
+  'node lib/cli.js spec/suitesConf.js'
+];
+
+scripts.push(
+    'node node_modules/.bin/minijasminenode jasminewd/spec/adapterSpec.js ' +
+    glob('spec/unit/*.js').join(' ') + ' ' +
+    glob('docs/spec/*.js').join(' '));
+
+var failed = false;
+
+(function runTests(i) {
+  if (i < scripts.length) {
+    console.log('node ' + scripts[i]);
+    var args = scripts[i].split(/\s/);
+
+    var test = spawn(args[0], args.slice(1), {stdio: 'inherit'});
+    test.on('error', function(err) {
+      throw err;
+    });
+    test.on('exit', function(code) {
+      if (code != 0) {
+        failed = true;
+      }
+      runTests(i + 1);
+    });
+  } else {
+    process.exit(failed ? 1 : 0);
+  }
+}(0));
