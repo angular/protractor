e20530e540c1d7572a1f7ed2e886511c1a9cfa3d
Hank Duan <duan.hank@gmail.com>
2014-06-04 13:53:29 -0700

    chore(test): remove shardingConf.js from test.js

--- scripts/test.js
+++ scripts/test.js
@@ -6,7 +6,6 @@ var spawn = require('child_process').spawn;
 var scripts = [
   'node lib/cli.js spec/basicConf.js',
   'node lib/cli.js spec/multiConf.js',
-  'node lib/cli.js spec/shardingConf.js',
   'node lib/cli.js spec/altRootConf.js',
   'node lib/cli.js spec/onPrepareConf.js',
   'node lib/cli.js spec/onPrepareFileConf.js',
