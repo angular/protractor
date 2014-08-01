8cb95547d86006c66e55f28e18b7e6da77264a3d
Julie Ralph <ju.ralph@gmail.com>
2014-08-01 15:00:53 -0700

    feat(runner): allow onPrepare functions to return a promise
    If onPrepare is a function which returns a promise (or a file which exports
    a promise), the test runner will now wait for that promise to be fulfilled
    before starting tests.

---
+++ spec/onPreparePromiseConf.js
@@ -0,0 +1,23 @@
+// Configuration using a function in onPrepare to set a parameter before
+// testing.
+var env = require('./environment.js');
+var q = require('q');
+
+// The main suite of Protractor tests.
+exports.config = {
+  seleniumAddress: env.seleniumAddress,
+
+  specs: [
+    'onPrepare/*_spec.js'
+  ],
+
+  capabilities: env.capabilities,
+
+  baseUrl: env.baseUrl,
+
+  onPrepare: function() {
+    return q.fcall(function() {
+      browser.params.password = '12345';
+    }).delay(1000);
+  }
+};
---
+++ spec/onPreparePromiseFileConf.js
@@ -0,0 +1,18 @@
+var env = require('./environment.js');
+
+// Configuration using a string in onPrepare to load a file with code to
+// execute once before tests.
+exports.config = {
+  seleniumAddress: env.seleniumAddress,
+
+  // Spec patterns are relative to this directory.
+  specs: [
+    'onPrepare/*_spec.js'
+  ],
+
+  capabilities: env.capabilities,
+
+  baseUrl: env.baseUrl,
+
+  onPrepare: 'onPrepare/asyncstartup.js'
+};
