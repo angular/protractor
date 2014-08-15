4855bc2d8ef496d791e7b12d6864471578e79804
andres <code@karateca.com>
2014-08-15 16:50:40 -0400

    feat(website): Add scenarios for Protractor Setup.

--- website/test/e2e/menu-partial.js
+++ website/test/e2e/menu-partial.js
@@ -3,10 +3,28 @@
  * @constructor
  */
 var MenuPartial = function() {
+  /**
+   * Get the labels for all the top level menu items.
+   * @return A promise that resolves to an array of strings.
+   */
   this.getTopMenuItems = function() {
     return $$('.navbar-nav > li > a').map(function(item) {
       return item.getText();
     });
+  };
+
+  this.dropdown = function(dropdownName) {
+    return {
+      item: function(itemName) {
+        $('.navbar-nav').
+            element(by.linkText(dropdownName)).
+            click();
+
+        $('.dropdown.open').
+            element(by.linkText(itemName)).
+            click();
+      }
+    };
   };
 };
 
--- website/test/e2e/navigation_spec.js
+++ website/test/e2e/navigation_spec.js
@@ -1,19 +1,49 @@
+/** @type {MenuPartial} */
 var menu = require('./menu-partial');
 
 ddescribe('Navigation', function() {
-  it('should go to home', function() {
+  beforeEach(function() {
     browser.get('#');
-    
+  });
+
+  it('should go to home', function() {
     expect($('.protractor-logo').isPresent()).toBe(true);
   });
 
   it('should have menu items', function() {
+    // Make sure all the top level menu labels are present.
     expect(menu.getTopMenuItems()).toEqual([
-        'Home',
-        'Quick start',
-        'Protractor Setup',
-        'Protractor Tests',
-        'Reference'
-    ])
+      'Home',
+      'Quick Start',
+      'Protractor Setup',
+      'Protractor Tests',
+      'Reference'
+    ]);
+  });
+
+  it('should go to tutorial', function() {
+    menu.dropdown('Quick Start').item('Tutorial');
+
+    expect($('h1').getText()).toBe('Tutorial');
+  });
+
+  describe('Protractor Setup', function() {
+    it('should go to Setting Up Protractor', function() {
+      menu.dropdown('Protractor Setup').item('Setting Up Protractor');
+
+      expect($('h1').getText()).toBe('Getting Installed');
+    });
+    
+    it('should go to Setting Up the Selenium Server', function() {
+      menu.dropdown('Protractor Setup').item('Setting Up the Selenium Server');
+
+      expect($('h1').getText()).toBe('Setting Up the Selenium Server');
+    });
+
+    it('should go to Setting Up the Browser', function() {
+      menu.dropdown('Protractor Setup').item('Setting Up the Browser');
+
+      expect($('h1').getText()).toBe('Setting Up the Browser');
+    });
   });
 });
