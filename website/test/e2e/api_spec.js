37c08cf699c6b19b9b13fc8db715186326fa9df0
andres <code@karateca.com>
2014-08-15 15:11:43 -0400

    feat(website): Add should search and find map.

--- website/test/e2e/api_spec.js
+++ website/test/e2e/api_spec.js
@@ -7,9 +7,23 @@ describe('Api', function() {
     expect($('#title').getText()).toBe('Protractor API 1.0.0');
   });
 
-  it('should show element.all() functions', function() {
+  it('should show element.all()', function() {
     var present = $('.api-left-nav').
         element(by.linkText('element.all(locator)')).isPresent();
     expect(present).toBe(true);
+  });
+
+  it('should search and find map', function() {
+    $('#searchInput').sendKeys('map');
+
+    // Ensure the following elements are shown:
+    // element.all
+    // map
+    $('.api-left-nav').$$('li').map(function(item) {
+      return item.getText();
+    }).then(function(items) {
+      expect(items[0]).toBe('element.all(locator)');
+      expect(items[1]).toBe('map');
+    });
   });
 });
