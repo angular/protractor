1ad91c51bb1ceb643aace4f82d47eececa140843
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2014-08-27 10:54:20 +0300

    Need to return promise from onPrepare hook.

--- spec/withLoginConf.js
+++ spec/withLoginConf.js
@@ -23,7 +23,7 @@ exports.config = {
     // Login takes some time, so wait until it's done.
     // For the test app's login, we know it's done when it redirects to
     // index.html.
-    browser.driver.wait(function() {
+    return browser.driver.wait(function() {
       return browser.driver.getCurrentUrl().then(function(url) {
         return /index/.test(url);
       });
