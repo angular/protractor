c8cb3bc49f05225d96cf7e277983015935ed6cc7
Julie Ralph <ju.ralph@gmail.com>
2014-08-28 12:08:07 -0700

    chore(website): remove old docgen folder

--- docgen/filters/link-slugify.js
+++
@@ -1,8 +0,0 @@
-// Transform links & anchors like 'elementFinder.isPresent' into
-// 'api-elementfinder-ispresent'.
-module.exports = {
-  name: 'slugify',
-  process: function(str) {
-    return 'api-' + str.replace(/\./g, '-').toLocaleLowerCase();
-  }
-};
