29a34afc51d151eac890c9ba380cbc817cfa9f19
Sammy Jelin <sjelin@gmail.com>
2014-12-08 16:28:54 -0800

    chore(docs): automatic doc generation, closes #1255

---
+++ scripts/generate-docs.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+#Check that directory is clean
+cd "$( dirname "${BASH_SOURCE[0]}" )/../website"
+if [ $(git status --porcelain | wc -l) != "0" ]; then
+  echo
+  echo "We cannot push the generated docs unless the working directory is"
+  echo "clean.  Either commit your changes, stash them, or generate the docs"
+  echo "manually by running gulp in /website/ and push them to gh-pages"
+  echo "at a later date"
+  echo
+  exit 1
+fi
+
+#Generate files
+gulp
+if [ $? -ne 0 ]; then
+  echo
+  echo "-----------------------------------------------------------------------"
+  echo "Gulp failed.  If you got a message that you were missing a required"
+  echo "package, try running the following in /website/:"
+  echo
+  echo "npm install del dgeni-packages dgeni gulp gulp-concat gulp-connect \\"
+  echo "    gulp-less gulp-markdown gulp-minify-css gulp-rename gulp-replace"
+  echo
+  exit 1
+fi
+
+#Transfer files to gh-pages
+cd ".."
+EXEC_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+git branch -D gh-pages
+git branch gh-pages
+git checkout gh-pages
+git pull https://github.com/angular/protractor.git gh-pages:gh-pages -f
+git reset --hard
+cp -r website/build/* .
+git add -A
+git commit -m "chore(website): automatic docs update"
+git checkout "${EXEC_BRANCH}"
