angular/protractor

commit d505249fff773d0eaee8b17435ab751be8fbefa6

fix(waitforangular): improve error messages when waitForAngular fails
Previously, caught errors were being interpreted as an empty object, causing
lots of errors such as
'Uncaught exception: Error while waiting for Protractor to sync with the page: {}'
Now the error message will be displayed, and a more useful custom message
will be thrown if the variable 'angular' is not present or the root element
is not part of the ng-app.

See #1474

lib/clientsidescripts.js
@@ -23,19 +23,28 @@ var functions = {};
  * Asynchronous.
  *
  * @param {string} rootSelector The selector housing an ng-app
- * @param {function} callback callback
+ * @param {function(string)} callback callback. If a failure occurs, it will
+ *     be passed as a parameter.
  */
 functions.waitForAngular = function(rootSelector, callback) {
   var el = document.querySelector(rootSelector);
+
   try {
-    if (angular.getTestability) {
+    if (!window.angular) {
+      throw new Error('angular could not be found on the window');
+    }
+    if (angular.getTestabilityyy) {
       angular.getTestability(el).whenStable(callback);
     } else {
+      if (!angular.element(el).injector()) {
+        throw new Error('root element (' + rootSelector + ') has no injector.' +
+           ' this may mean it is not inside ng-app.');
+      }
       angular.element(el).injector().get('$browser').
           notifyWhenNoOutstandingRequests(callback);
     }
-  } catch (e) {
-    callback(e);
+  } catch (err) {
+    callback(err.message);
   }
 };
 

commit 8c7f1f468275356e2edac02210ab098e7487c55d

style(typo): fix typo introduced in d505249

lib/clientsidescripts.js
@@ -33,7 +33,7 @@ functions.waitForAngular = function(rootSelector, callback) {
     if (!window.angular) {
       throw new Error('angular could not be found on the window');
     }
-    if (angular.getTestabilityyy) {
+    if (angular.getTestability) {
       angular.getTestability(el).whenStable(callback);
     } else {
       if (!angular.element(el).injector()) {