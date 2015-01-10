28bda1bd3a85b3d98dadb38a0398469b6dfd76a8
Andres Dominguez <andres.dominguez.p@gmail.com>
2015-01-10 18:57:37 -0500

    chore(website): Upgrade e2e tests to 1.6.0 API
    Upgrade the e2e tests to reflect the new 1.6.0 API

--- website/test/e2e/navigation_spec.js
+++ website/test/e2e/navigation_spec.js
@@ -163,6 +163,19 @@ describe('Navigation', function() {
 
       expect($('h1').getText()).toBe('How It Works');
     });
+    
+    it('should go to Upgrading to Jasmine 2.0', function() {
+      menu.dropdown('Reference').item('Upgrading to Jasmine 2.0');
+
+      expect($('h1').getText()).toBe('Upgrading from Jasmine 1.3 to 2.x');
+    });
+    
+    it('should go to Mobile Setup', function() {
+      menu.dropdown('Reference').item('Mobile Setup');
+
+      expect($('h1').getText()).toBe('Mobile Setup');
+
+    });
 
     it('should go to FAQ', function() {
       menu.dropdown('Reference').item('FAQ');
