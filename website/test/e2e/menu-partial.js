25e4f7ede7652ec96eb53285d9ef9503aa94636d
Andres Dominguez <andres.dominguez.p@gmail.com>
2015-01-10 19:50:53 -0500

    fix(website): Fix broken link in Home menu link.
    Fix the link in the Home menu item to go to homepage.

--- website/test/e2e/menu-partial.js
+++ website/test/e2e/menu-partial.js
@@ -3,15 +3,7 @@
  * @constructor
  */
 var MenuPartial = function() {
-  /**
-   * Get the labels for all the top level menu items.
-   * @return A promise that resolves to an array of strings.
-   */
-  this.getTopMenuItems = function() {
-    return $$('.navbar-nav > li > a').map(function(item) {
-      return item.getText();
-    });
-  };
+  this.topMenuItems = $$('.navbar-nav > li > a');
 
   /**
    * Dropdown api. Used to click on an element under a dropdown or to get the
@@ -20,39 +12,35 @@ var MenuPartial = function() {
    * @return {{item: Function, itemNames: Function}}
    */
   this.dropdown = function(dropdownName) {
-
-    /**
-     * Open a dropdown given its name.
-     * @private
-     */
-    var openDropdown_ = function() {
-      $('.navbar-nav').
-          element(by.linkText(dropdownName)).
-          click();
-    };
-
     return {
+      /**
+       * Open a dropdown given its name.
+       */
+      open: function() {
+        $('.navbar-nav')
+            .element(by.linkText(dropdownName))
+            .click();
+      },
       /**
        * Select an item under a menu dropdown.
        * @param {string} itemName
        */
       item: function(itemName) {
-        openDropdown_();
+        this.open();
 
         // Click on an element under the open dropdown.
-        $('.dropdown.open').
-            element(by.linkText(itemName)).
-            click();
+        $('.dropdown.open')
+            .element(by.linkText(itemName))
+            .click();
       },
       /**
        * Get the names of the items under a dropdown menu.
        */
       itemNames: function() {
-        openDropdown_();
+        this.open();
 
-        return $$('.dropdown.open .dropdown-menu li a').map(function(item) {
-          return item.getText();
-        });
+        return $$('.dropdown.open .dropdown-menu li a')
+            .getText();
       }
     };
   };
