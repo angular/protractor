f2de2c2ce35fc2212a4c21bdc9cb75d767cd8e0a
Leo Gallucci <elgalu3@gmail.com>
2015-01-23 11:06:00 -0300

    feat(frameworks): add support for custom frameworks
    Usage:
    ```js
    exports.config = {
      framework: 'custom',
      frameworkPath: '/path/to/your/framework/index.js'
    }
    ```

---
+++ spec/custom/smoke_spec.js
@@ -0,0 +1,5 @@
+describe('smoke jasmine tests', function() {
+  it('should do some dummy test', function() {
+    expect(1).toBe(1);
+  });
+});
---
+++ spec/customFramework.js
@@ -0,0 +1,16 @@
+var env = require('./environment.js');
+
+exports.config = {
+  seleniumAddress: env.seleniumAddress,
+
+  framework: 'custom',
+  frameworkPath: './custom/framework.js',
+
+  specs: [
+    'custom/smoke_spec.js'
+  ],
+
+  capabilities: env.capabilities,
+
+  baseUrl: env.baseUrl,
+};
