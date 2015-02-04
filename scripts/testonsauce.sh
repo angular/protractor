c1039eda1b0322dfa3881cbbf7881b4d742e37dd
Sammy Jelin <sjelin@gmail.com>
2015-02-04 15:28:18 -0800

    tests(node version): only use node 0.11.* in smoke tests

--- scripts/testonsauce.sh
+++ scripts/testonsauce.sh
@@ -3,7 +3,9 @@ SAUCE_ACCESS_KEY=`echo $SAUCE_ACCESS_KEY | rev`
 if [ $JOB = "smoke" ]; then
   node bin/protractor spec/smokeConf.js
 elif [ $JOB = "suite" ]; then
-  node bin/protractor spec/ciConf.js
+  if [[ `node -v` == v0.10.* ]]; then
+    node bin/protractor spec/ciConf.js
+  fi
 else
   echo "Unknown job type. Please set JOB=smoke or JOB=suite"
 fi
