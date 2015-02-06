072f9a73a6349be92f7e97e2970eb38b1eb58b0c
Julie Ralph <ju.ralph@gmail.com>
2015-02-05 21:50:21 -0800

    chore(ngHint): move ngHint plugin to a consistent structure
    Also, fixed up the comments on how to use.

--- plugins/ngHint/spec/successConfig.js
+++ plugins/ngHint/spec/successConfig.js
@@ -3,9 +3,9 @@ var env = require('./environment.js');
 exports.config = {
   seleniumAddress: env.seleniumAddress,
   framework: 'jasmine2',
-  specs: ['ngHint/success_spec.js'],
+  specs: ['success_spec.js'],
   baseUrl: env.baseUrl,
   plugins: [{
-    path: "../plugins/ngHintPlugin.js"
+    path: "../index.js"
   }]
 };
