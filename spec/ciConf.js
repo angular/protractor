b0de4ce6a3e2d7f602a7693d5f8e061756b4f1cb
Julie Ralph <ju.ralph@gmail.com>
2015-02-18 22:25:38 -0800

    WIP - testing an 'error' exit code

--- spec/ciConf.js
+++ spec/ciConf.js
@@ -9,7 +9,7 @@ exports.config = {
 
   // Spec patterns are relative to this directory.
   specs: [
-    'basic/*_spec.js'
+    'basic/lib_spec.js'
   ],
 
   // Exclude patterns are relative to this directory.
