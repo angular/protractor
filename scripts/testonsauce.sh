d26dc641056e535a743989b30c56a2f28c37da56
Julie Ralph <ju.ralph@gmail.com>
2015-02-27 12:56:30 -0800

    chore(ci): exclude node 0.12 suite tests instead of suppressing

--- scripts/testonsauce.sh
+++ scripts/testonsauce.sh
@@ -3,9 +3,7 @@ SAUCE_ACCESS_KEY=`echo $SAUCE_ACCESS_KEY | rev`
 if [ $JOB = "smoke" ]; then
   node bin/protractor spec/smokeConf.js
 elif [ $JOB = "suite" ]; then
-  if [[ `node -v` == v0.10.* ]]; then
-    node bin/protractor spec/ciConf.js
-  fi
+  node bin/protractor spec/ciConf.js
 else
   echo "Unknown job type. Please set JOB=smoke or JOB=suite"
 fi
