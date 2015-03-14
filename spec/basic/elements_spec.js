069607ff04fca1e09840ac375b9b8b07f5b82955
Julie Ralph <ju.ralph@gmail.com>
2015-03-13 17:37:32 -0700

    fix(element): update to selenium-webdriver@2.45.1 and remove element.then
    This change updates the version of WebDriverJS (selenium-webdriver node
    module) from 2.44 to 2.45.1. See the full changelog at
    https://github.com/SeleniumHQ/selenium/blob/master/javascript/node/selenium-webdriver/CHANGES.md
    
    To enable the update and remove confusion, this removes the `element().then`
    function unless there is an action result. This function is completely
    unnecessary, because it would always resolve to itself, but the removal
    may cause breaking changes.
    
    Before:
    ```js
    element(by.css('foo')).then(function(el) {
      return el.getText().then(...);
    });
    ```
    
    After:
    ```js
    element(by.css('foo')).getText().then(...);
    ```
    
    In other words, an ElementFinder is now no longer a promise until an action
    has been called.
    
    Before:
    ```js
    var el = element(by.css('foo'));
    
    protractor.promise.isPromise(el); // true
    protractor.promise.isPromise(el.click()); // true
    ```
    
    After:
    ```js
    var el = element(by.css('foo'));
    
    protractor.promise.isPromise(el); // false
    protractor.promise.isPromise(el.click()); // true
    ```
    
    Also, fixes `filter` and `map` to work with the new WebDriverJS.

--- spec/basic/elements_spec.js
+++ spec/basic/elements_spec.js
@@ -157,28 +157,10 @@ describe('ElementFinder', function() {
     expect(successful).toEqual(false);
   });
 
-  it('then function should be equivalent to itself', function() {
-    browser.get('index.html#/form');
-    var elem = element(by.binding('greeting'));
-
-    elem.then(function(elem2) {
-      expect(elem.getId()).toEqual(elem2.getId());
-    });
-  });
-
-  it('should not resolve to itself', function() {
-    browser.get('index.html#/form');
-    var elem1 = element(by.binding('greeting'));
-
-    elem1.then(function(result) {
-      expect(result === elem1).toBe(false);
-    });
-  });
-
   it('should be returned from a helper without infinite loops', function() {
     browser.get('index.html#/form');
-    var helperPromise = element(by.binding('greeting')).then(function(result) {
-      return result;
+    var helperPromise = protractor.promise.when(true).then(function() {
+      return element(by.binding('greeting'));
     });
 
     helperPromise.then(function(finalResult) {
@@ -186,7 +168,7 @@ describe('ElementFinder', function() {
     });
   });
 
-  it('should be usable in WebDriver functions', function() {
+  it('should be usable in WebDriver functions via getWebElement', function() {
     // TODO(juliemr): should be able to do this without the getWebElement call
     browser.get('index.html#/form');
     var greeting = element(by.binding('greeting'));
@@ -509,16 +491,11 @@ describe('ElementArrayFinder', function() {
                           '4/5: small cat\n');
   });
 
-  it('should always return a promise when calling then', function() {
-    browser.get('index.html#/form');
-    var e1 = element(by.tagName('body')).then(function() {});
-    expect(e1 instanceof protractor.promise.Promise).toBe(true);
-  });
-
   it('should allow using protractor locator within map', function() {
     browser.get('index.html#/repeater');
 
-    var expected = [{ first: 'M', second: 'Monday' },
+    var expected = [
+        { first: 'M', second: 'Monday' },
         { first: 'T', second: 'Tuesday' },
         { first: 'W', second: 'Wednesday' },
         { first: 'Th', second: 'Thursday' },
