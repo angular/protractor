02c18e1d942442a4f2e0602e77efd5a00492605f
Julie Ralph <ju.ralph@gmail.com>
2015-04-10 14:50:45 -0700

    chore(ci): version bumps for chrome, ff, and safari

--- spec/ciFullConf.js
+++ spec/ciFullConf.js
@@ -22,7 +22,7 @@ exports.config = {
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER,
     'name': 'Protractor suite tests',
-    'version': '40',
+    'version': '41',
     'selenium-version': '2.45.0',
     'chromedriver-version': '2.14',
     'platform': 'OS X 10.9'
@@ -31,7 +31,7 @@ exports.config = {
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER,
     'name': 'Protractor suite tests',
-    'version': '35',
+    'version': '37',
     'selenium-version': '2.45.0'
   }],
 
