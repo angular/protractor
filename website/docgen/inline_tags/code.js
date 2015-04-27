b2a3c96710e4ebec2350458ab2c9df162702209d
Sammy Jelin <sjelin@gmail.com>
2015-04-27 11:11:24 -0700

    bug(locators): escape query in byExactBinding
    See http://stackoverflow.com/q/3561711
    
    Closes #1918

--- website/docgen/inline_tags/code.js
+++ website/docgen/inline_tags/code.js
@@ -1,8 +1,20 @@
+var htmlSpecialCharMap = {
+  "&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+  '"': '&quot;',
+  "'": '&#39;',
+  "/": '&#x2F;'
+};
+
 module.exports = function codeTagDef() {
   return {
     name: 'code',
     handler: function(doc, tag, content) {
-      return '<code ng-non-bindable>' + content + '</code>';
+      return '<code ng-non-bindable>' +
+          content.replace(/[&<>"'\/]/g, function (s) { 
+            return htmlSpecialCharMap[s];
+          }) + '</code>';
     },
     description: 'Handle inline code tags',
     aliases: ['monospace']
