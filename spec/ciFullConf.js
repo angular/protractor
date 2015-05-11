30df32c4d6450c91c841095af3f915a5091ffcc4
Sammy Jelin <sjelin@gmail.com>
2015-05-11 10:58:55 -0700

    chore(tests): chrome version bump

--- spec/ciFullConf.js
+++ spec/ciFullConf.js
@@ -22,7 +22,7 @@ exports.config = {
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER,
     'name': 'Protractor suite tests',
-    'version': '41',
+    'version': '42',
     'selenium-version': '2.45.0',
     'chromedriver-version': '2.14',
     'platform': 'OS X 10.9'
