6121c9181ebccf4e0f2e47d8c741161fb1f747df
Sammy Jelin <sjelin@gmail.com>
2015-05-11 10:58:55 -0700

    chore(tests): chrome version bump

--- spec/ciSmokeConf.js
+++ spec/ciSmokeConf.js
@@ -21,7 +21,7 @@ exports.config = {
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER,
     'name': 'Protractor smoke tests',
-    'version': '40',
+    'version': '42',
     'selenium-version': '2.45.0',
     'chromedriver-version': '2.14',
     'platform': 'OS X 10.9'
