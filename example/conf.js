6b372d94b68e1f337d37488f08c9567e9647232f
Julie Ralph <ju.ralph@gmail.com>
2015-05-18 12:49:26 -0700

    chore(example): change example configuration to use jasmine2

--- example/conf.js
+++ example/conf.js
@@ -7,13 +7,15 @@ exports.config = {
     'browserName': 'chrome'
   },
 
+  // Framework to use. Jasmine 2 is recommended.
+  framework: 'jasmine2',
+
   // Spec patterns are relative to the current working directly when
   // protractor is called.
   specs: ['example_spec.js'],
 
-  // Options to be passed to Jasmine-node.
+  // Options to be passed to Jasmine.
   jasmineNodeOpts: {
-    showColors: true,
     defaultTimeoutInterval: 30000
   }
 };
