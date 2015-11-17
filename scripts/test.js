d57bdc2d3ba879c90d8ca01b3c0b5f8ab7e4ded4
Julie Ralph <ju.ralph@gmail.com>
2015-11-17 15:07:22 -0800

    fix(tests): fix error tests after testapp api slight rename x /slowcall -> slowcall

--- scripts/test.js
+++ scripts/test.js
@@ -110,7 +110,7 @@ executor.addCommandlineTest('node lib/cli.js spec/errorTest/pluginsFailingConf.j
 executor.addCommandlineTest('node lib/cli.js spec/errorTest/slowHttpAndTimeoutConf.js')
     .expectExitCode(1)
     .expectErrors([
-      {message: 'The following tasks were pending[\\s\\S]*\\$http: \/slowcall'},
+      {message: 'The following tasks were pending[\\s\\S]*\\$http: slowcall'},
       {message: 'The following tasks were pending[\\s\\S]*' +
                 '\\$timeout: function \\(\\) {[\\s\\S]*' +
                   '\\$scope\\.slowAngularTimeoutStatus = \'done\';[\\s\\S]' +
@@ -121,7 +121,7 @@ executor.addCommandlineTest('node lib/cli.js spec/errorTest/slowHttpAndTimeoutCo
                             '--untrackOutstandingTimeouts true')
     .expectExitCode(1)
     .expectErrors([
-      {message: 'The following tasks were pending[\\s\\S]*\\$http: \/slowcall'},
+      {message: 'The following tasks were pending[\\s\\S]*\\$http: slowcall'},
       {message: '^((?!The following tasks were pending).)*$'}
     ]);
 
