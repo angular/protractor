7ce4919740cfbc53dfd0e9c8e9c258af257e8381
Craig Nishina <craig.nishina@gmail.com>
2016-01-29 12:39:28 -0800

    chore(test): fix npm test to install missing prerequisites

--- scripts/pretest.sh
+++ scripts/pretest.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # When running `npm test`, this script is intended to run during pretest step.
 
-WS=$(pwd)                          # the protractor directory
-$WS/bin/webdriver-manager update   # updates the local webdriver manager
-$WS/node_modules/.bin/jshint lib spec scripts
-cd $WS/website && npm install      # installs dependencies for the website
+bin/webdriver-manager update   # updates the local webdriver manager
+node_modules/.bin/jshint lib spec scripts
