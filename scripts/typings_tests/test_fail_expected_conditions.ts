b515ae87cbb863331c80f68403d54f9d5e614f46
Craig <cnishina@users.noreply.github.com>
2016-06-21 18:24:46 -0700

    chore(test): add tests for declaration type file (#3297)

---
+++ scripts/typings_tests/test_fail_expected_conditions.ts
@@ -0,0 +1,24 @@
+/// <reference path="../../built/index.d.ts" />
+import {element, by, ExpectedConditions} from 'protractor';
+ExpectedConditions.not(0);
+ExpectedConditions.not('1');
+ExpectedConditions.not(true);
+ExpectedConditions.and(0);
+ExpectedConditions.and('1');
+ExpectedConditions.and(true);
+ExpectedConditions.or(0, () => {});
+ExpectedConditions.or('1', () => {});
+ExpectedConditions.or(true, () => {});
+ExpectedConditions.or(() => {}, 0);
+ExpectedConditions.or(() => {}, '1');
+ExpectedConditions.or(() => {}, true);
+ExpectedConditions.or(0, '1');
+ExpectedConditions.alertIsPresent(0);
+ExpectedConditions.alertIsPresent('1');
+ExpectedConditions.alertIsPresent(true);
+ExpectedConditions.alertIsPresent(() => {});
+ExpectedConditions.elementToBeClickable(0);
+ExpectedConditions.elementToBeClickable('1');
+ExpectedConditions.elementToBeClickable();
+ExpectedConditions.elementToBeClickable(true);
+ExpectedConditions.elementToBeClickable(() => {});
