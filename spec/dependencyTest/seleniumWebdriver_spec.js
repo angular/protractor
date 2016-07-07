936dddd8110f38cfa736bd9129d06338c80605f2
Craig <cnishina@users.noreply.github.com>
2016-07-07 12:53:24 -0700

    chore(selenium): add a dependency test for selenium-webdriver (#3336)

---
+++ spec/dependencyTest/seleniumWebdriver_spec.js
@@ -0,0 +1,131 @@
+var WebDriver = require('selenium-webdriver').WebDriver;
+var By = require('selenium-webdriver').By;
+var Setup = require('./setup');
+var Chrome = require('selenium-webdriver/chrome');
+var Firefox = require('selenium-webdriver/firefox');
+var Executors = require('selenium-webdriver/executors');
+var Error = require('selenium-webdriver').error;
+var Remote = require('selenium-webdriver/remote');
+var Testing = require('selenium-webdriver/testing');
+
+const WEBDRIVER = {
+  staticFunctions: ['attachToSession', 'createSession'],
+  instanceFunctions: ['actions', 'wait', 'sleep', 'getCurrentUrl', 'getTitle',
+    'takeScreenshot', 'getSession', 'getCapabilities', 'quit', 'touchActions',
+    'executeAsyncScript', 'call', 'wait', 'getWindowHandle',
+    'getAllWindowHandles', 'getPageSource', 'close', 'get', 'findElement',
+    'isElementPresent', 'findElements', 'manage', 'navigate', 'switchTo']
+};
+const WEBELEMENT = {
+  instanceFunctions: ['getDriver', 'getId', 'getRawId',
+    'findElement', 'click', 'sendKeys', 'getTagName', 'getCssValue',
+    'getAttribute', 'getText', 'getSize', 'getLocation', 'isEnabled',
+    'isSelected', 'submit', 'clear', 'isDisplayed', 'takeScreenshot']
+};
+const BY = {
+  staticFunctions: ['className', 'css', 'id', 'linkText', 'js', 'name',
+      'partialLinkText', 'tagName', 'xpath']
+};
+const SESSION = {
+  instanceFunctions: ['getId', 'getCapabilities']
+};
+const CHROME = {
+  staticFunctions: ['Driver', 'ServiceBuilder']
+};
+const FIREFOX = {
+  staticFunction: 'Driver'
+}
+const EXECUTORS = {
+  staticFunction: 'createExecutor'
+}
+const TESTING = {
+  staticFunctions: ['after', 'afterEach', 'before', 'beforeEach',
+      'describe', 'it', 'iit']
+}
+
+describe('selenium-webdriver dependency', function() {
+  describe('require("selenium-webdriver").WebDriver', function() {
+    for (var pos1 in WEBDRIVER.staticFunctions) {
+      var staticFunc = WEBDRIVER.staticFunctions[pos1];
+      it('should have a ' + staticFunc + ' function', function() {
+        expect(typeof WebDriver[staticFunc] == 'function').toBe(true);
+      });
+    }
+
+    var webdriver = Setup.getWebDriver();
+    for (var pos2 in WEBDRIVER.instanceFunctions) {
+      var instanceFunc = WEBDRIVER.instanceFunctions[pos2];
+      it('should have a ' + instanceFunc + ' function', function() {
+        expect(typeof webdriver[instanceFunc] == 'function').toBe(true);
+      });
+    }
+  });
+  describe('require("selenium-webdriver").WebElement', function() {
+    var webElement = Setup.getWebElement();
+    for (var pos in WEBELEMENT.instanceFunctions) {
+      var func = WEBELEMENT.instanceFunctions[pos];
+      it('should have a ' + func + ' function', function() {
+        expect(typeof webElement[func] == 'function').toBe(true);
+      });
+    }
+  });
+  describe('require("selenium-webdriver").By', function() {
+    for (var pos in BY.staticFunctions) {
+      var func = BY.staticFunctions[pos];
+      it('should have a ' + func + ' function', function() {
+        expect(typeof By[func] == 'function').toBe(true);
+      });
+    }
+  });
+  describe('require("selenium-webdriver").Session', function() {
+    var session = Setup.getSession();
+    for (var pos in SESSION.instanceFunctions) {
+      var func = SESSION.instanceFunctions[pos];
+      it('should have a ' + func + ' function', function() {
+        expect(typeof session[func] == 'function').toBe(true);
+      });
+    }
+  });
+  describe('require("selenium-webdriver/chrome")', function() {
+    for (var pos in CHROME.staticFunctions) {
+      var func = CHROME.staticFunctions[pos];
+      it('should have a ' + func + ' function', function() {
+        expect(typeof Chrome[func] == 'function').toBe(true);
+      });
+    }
+  });
+  describe('require("selenium-webdriver/firefox")', function() {
+    it('should have a ' + FIREFOX.staticFunction + ' function', function() {
+      expect(typeof Firefox[FIREFOX.staticFunction] == 'function').toBe(true);
+    });
+  });
+  describe('require("selenium-webdriver/executors")', function() {
+    it('should have a ' + EXECUTORS.staticFunction + ' function', function() {
+      expect(typeof Executors[EXECUTORS.staticFunction] == 'function').toBe(true);
+    })
+  });
+  describe('require("selenium-webdriver").error', function() {
+    it('should have a NoSuchElementError function', function() {
+      expect(typeof Error.NoSuchElementError == 'function').toBe(true);
+    });
+    it('should have error codes', function() {
+      expect(typeof Error.ErrorCode == 'object').toBe(true);
+    });
+    it('should have an error code of NO_SUCH_ALERT', function() {
+      expect(typeof Error.ErrorCode.NO_SUCH_ALERT == 'number').toBe(true);
+    });
+  });
+  describe('require("selenium-webdriver/remote")', function() {
+    it('should have a SeleniumServer function', () => {
+      expect(typeof Remote['SeleniumServer'] == 'function').toBe(true);
+    });
+  });
+  describe('require("selenium-webdriver/testing")', function() {
+    for (var pos in TESTING.staticFunctions) {
+      var func = TESTING.staticFunctions[pos];
+      it('should have a ' + func + ' function', () => {
+        expect(typeof Testing[func] == 'function').toBe(true);
+      });
+    }
+  });
+});
