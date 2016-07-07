a52207892b773dcd03e87543916ca10a6cf007f6
Craig <cnishina@users.noreply.github.com>
2016-07-07 15:39:52 -0700

    chore(cleanup): clean up for requiring selenium-webdriver (#3345)

--- lib/ptor.ts
+++ lib/ptor.ts
@@ -17,21 +17,20 @@ export namespace protractor {
 
   // Define selenium webdriver imports.
   export let promise = {
-    controlFlow: require('selenium-webdriver/lib/promise').controlFlow,
-    createFlow: require('selenium-webdriver/lib/promise').createFlow,
-    defer: require('selenium-webdriver/lib/promise').defer,
-    delayed: require('selenium-webdriver/lib/promise').delayed,
-    filter: require('selenium-webdriver/lib/promise').filter,
-    fulfilled: require('selenium-webdriver/lib/promise').fulfilled,
-    fullyResolved: require('selenium-webdriver/lib/promise').fullyResolved,
-    isPromise: require('selenium-webdriver/lib/promise').isPromise,
-    rejected: require('selenium-webdriver/lib/promise').rejected,
-    thenFinally: require('selenium-webdriver/lib/promise').thenFinally,
-    when: require('selenium-webdriver/lib/promise').when
+    controlFlow: webdriver.promise.controlFlow,
+    createFlow: webdriver.promise.createFlow,
+    defer: webdriver.promise.defer,
+    delayed: webdriver.promise.delayed,
+    filter: webdriver.promise.filter,
+    fulfilled: webdriver.promise.fulfilled,
+    fullyResolved: webdriver.promise.fullyResolved,
+    isPromise: webdriver.promise.isPromise,
+    rejected: webdriver.promise.rejected,
+    thenFinally: webdriver.promise.thenFinally,
+    when: webdriver.promise.when
   };
-  export let ActionSequence =
-      require('selenium-webdriver/lib/actions').ActionSequence;
-  export let Key = require('selenium-webdriver/lib/input').Key;
+  export let ActionSequence = webdriver.ActionSequence;
+  export let Key = webdriver.Key;
   export let Command = require('selenium-webdriver/lib/command').Command;
   export let CommandName = require('selenium-webdriver/lib/command').Name;
 }
