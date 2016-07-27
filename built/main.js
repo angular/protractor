3a9bbe0f0fa05df2b6f78b7dc8e46f5e589cea37
Craig Nishina <craig.nishina@gmail.com>
2016-07-27 13:11:29 -0700

    chore(website): automatic docs update for 4.0.1

---
+++ built/main.js
@@ -0,0 +1,6 @@
+/**
+ * Requiring protractor as a node module. Export the contents of the protractor
+ * namespace.
+ */
+"use strict";
+module.exports = require('./ptor').protractor;
---
+++ globals.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.protractor = global['protractor'];
+exports.browser = global['protractor']['browser'];
+exports.element = global['protractor']['element'];
+exports.by = global['protractor']['by'];
+exports.By = global['protractor']['By'];
+exports.$ = global['protractor']['$'];
+exports.$$ = global['protractor']['$$'];
+exports.ExpectedConditions = global['protractor']['ExpectedConditions'];
