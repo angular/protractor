22d537392375c63915c951168f0d82c41111426a
Craig Nishina <craig.nishina@gmail.com>
2016-08-24 13:34:15 -0700

    chore(tests): fixes to test suite
    - JavaScript file should use 'var' and not 'let'
    - Plugin tests should use '__BASIC_PLUGIN_RAN_SETUP'

--- spec/plugins/specs/fail_spec.js
+++ spec/plugins/specs/fail_spec.js
@@ -1,6 +1,6 @@
 describe('check if plugin setup ran', function() {
   it('should have set protractor.__BASIC_PLUGIN_RAN', function() {
-    expect(protractor.__BASIC_PLUGIN_RAN).toBe(true);
+    expect(protractor.__BASIC_PLUGIN_RAN_SETUP).toBe(true);
   });
 
   it('should run multiple tests which fail', function() {
