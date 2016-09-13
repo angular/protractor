7820d24d2b7f273122885fb807df4c495b3f7e83
Craig Nishina <craig.nishina@gmail.com>
2016-09-13 12:05:18 -0700

    chore(testapp): upgrade to angular 2.0.0-rc.7
    - remove hybrid test app test

--- testapp/hybrid/app/myApp.js
+++ testapp/hybrid/app/myApp.js
@@ -1,27 +1,19 @@
-System.register([], function(exports_1, context_1) {
-    "use strict";
-    var __moduleName = context_1 && context_1.id;
-    function ctrl($scope, $timeout) {
-        $scope.callCount = 0;
-        $scope.clickButton = function () {
-            $timeout(function () {
-                $scope.callCount++;
-            }, 1000);
-        };
-    }
-    function myApp() {
-        return {
-            scope: {},
-            templateUrl: './html/myApp.html',
-            controller: ctrl,
-            controllerAs: 'ctrl'
-        };
-    }
-    exports_1("myApp", myApp);
+"use strict";
+function ctrl($scope, $timeout) {
+    $scope.callCount = 0;
+    $scope.clickButton = function () {
+        $timeout(function () {
+            $scope.callCount++;
+        }, 1000);
+    };
+}
+function myApp() {
     return {
-        setters:[],
-        execute: function() {
-        }
-    }
-});
+        scope: {},
+        templateUrl: './html/myApp.html',
+        controller: ctrl,
+        controllerAs: 'ctrl'
+    };
+}
+exports.myApp = myApp;
 //# sourceMappingURL=myApp.js.map
--- testapp/ng2/app/app.component.ts
+++ testapp/ng2/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html',
-  directives: [ ROUTER_DIRECTIVES ]
+  templateUrl: 'app/app.component.html'
 })
 export class AppComponent {
 
