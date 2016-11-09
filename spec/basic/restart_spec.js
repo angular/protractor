7d7553334468401483387de642481287f2681491
Michael Giambalvo <heathkit@google.com>
2016-11-08 20:12:51 -0800

    Whoops

--- spec/basic/restart_spec.js
+++ spec/basic/restart_spec.js
@@ -1,10 +1,10 @@
-fdescribe('browser.restart', function() {
-  fit('doesn\'t break ignoreSynchronization', function() {
+describe('browser.restart', function() {
+  it('doesn\'t break ignoreSynchronization', function() {
     browser.get('index.html#/polling');
     browser.restart();
 
     browser.ignoreSynchronization = true;
     // Get a non-angular page. It shouldn't fail if ignoreSynchronization is on.
-    browser.get("https://google.com/");
+    browser.get('https://google.com/');
   });
 });
