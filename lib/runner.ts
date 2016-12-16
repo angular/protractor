8097201d9efe12a2b80153d27d9f1280bf3624c0
mgiambalvo <heathkit@gmail.com>
2016-12-16 10:08:28 -0800

    refactor(browser): Remove protractor.wrapDriver() **breaking change** (#3827)
    Before:
    Users could create their own selenium driver instance and enable Protractor on
    it like so:
    ```js
    let capabilities = webdriver.Capabilities.chrome();
    let driver = new webdriver.Builder().usingServer(seleniumAddress)
        .withCapabilities(capabilities).build();
    let browser = protractor.wrapDriver(driver);
    ```
    
    Over the years, wrapDriver() has become increasingly broken as Protractor
    needs extra configuration options that wrapDriver() doesn't set.
    
    After:
    This method is removed. If users need a new browser instance, they can
    use `browser.forkNewDriverInstance()`.

--- lib/runner.ts
+++ lib/runner.ts
@@ -164,7 +164,6 @@ export class Runner extends EventEmitter {
     protractor.$$ = browser_.$$;
     protractor.element = browser_.element;
     protractor.by = protractor.By = ProtractorBrowser.By;
-    protractor.wrapDriver = ProtractorBrowser.wrapDriver;
     protractor.ExpectedConditions = browser_.ExpectedConditions;
 
     if (!this.config_.noGlobals) {
@@ -205,7 +204,7 @@ export class Runner extends EventEmitter {
     let config = this.config_;
     let driver = this.driverprovider_.getNewDriver();
 
-    let browser_ = ProtractorBrowser.wrapDriver(
+    let browser_ = new ProtractorBrowser(
         driver, config.baseUrl, config.rootElement, config.untrackOutstandingTimeouts);
 
     browser_.params = config.params;
