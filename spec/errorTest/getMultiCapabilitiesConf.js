ab20e57d139faab33f7dc845c41a22882fd50ad0
Craig Nishina <craig.nishina@gmail.com>
2016-12-22 15:06:48 -0800

    fix(launcher): running getMultiCapabilities should reject on errors
    closes #3875

---
+++ spec/errorTest/getMultiCapabilitiesConf.js
@@ -0,0 +1,18 @@
+var env = require('../environment.js');
+
+exports.config = {
+  seleniumAddress: env.seleniumAddress,
+
+  // Spec patterns are relative to this directory.
+  specs: [
+    '../../basic/mock*'
+  ],
+
+  getMultiCapabilities: function() {
+    return new Promise((resolve) => {
+      throw new Error('get multi capabilities failed');
+    });
+  },
+
+  baseUrl: env.baseUrl + '/ng1/'
+};
