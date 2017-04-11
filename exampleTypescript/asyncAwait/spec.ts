1e176f4e0723542b97b0b74d53a77adbca15d8de
Michael Giambalvo <heathkit@google.com>
2017-04-11 11:49:08 +0200

    docs(await): Update example to show debugging with chrome devtools
    Change the async/await example to use ts-node. Also, when using
    SELENIUM_PROMISE_MANAGER=false, you can debug tests using
    `node --inspect` and the chrome debugger.
    
    Also, update the docs around timeouts to include Angular vs AngularJS

--- exampleTypescript/asyncAwait/spec.ts
+++ exampleTypescript/asyncAwait/spec.ts
@@ -3,10 +3,11 @@ import {browser, element, by, By, $, $$, ExpectedConditions} from 'protractor';
 
 describe('async function', function() {
   it('should wait on async function in conditional', async function() : Promise<any> {
-    browser.get('http://www.angularjs.org');
+    await browser.get('http://www.angularjs.org');
     let todoList = element.all(by.repeater('todo in todoList.todos'));
     if ((await todoList.count()) > 1) {
-      expect((await todoList.get(1).getText())).toEqual('build an angular app');
+      debugger
+      expect((await todoList.get(1).getText())).toEqual('build an AngularJS app');
     }
   });
 });
