61097607ea89239ab6fc27127b61b0b021c9528a
Craig Nishina <craig.nishina@gmail.com>
2017-07-29 15:21:54 -0700

    fix(local): allow local driver provider to use gecko driver from config
    - Add gecko driver as configuration option to be used in the local
      driver provider.
    - Nit fixes to use string[] over Array<string> in the configParser.ts.
    - Add functionality to addDefaultBinaryLocs_ to use the geckoDriver
      value set in the config or to check locally in the
      webdriver-manager/selenium folder.
    - Fix transpile errors in locator. Missing toString in ProtractorLocator
      interface.
    - Fix transpile errors in element. Cast wdpromise.Promise<{}> to
      wdpromise.Promise<T>.
    - xit spec/basic/action_spec.js based on
      [selenium-webdriver issue #3693](https://github.com/SeleniumHQ/selenium/issues/3693).
      Added a // TODO comment to remove xit when selenium-webdriver resolves issue.
    
    closes #4408 and closes #4411.

--- lib/config.ts
+++ lib/config.ts
@@ -66,15 +66,21 @@ export interface Config {
     jvmArgs?: string[];
   };
   /**
-   * ChromeDriver location is used to help find the chromedriver binary.
-   * This will be passed to the Selenium jar as the system property
-   * webdriver.chrome.driver. If null, Selenium will attempt to find
-   * ChromeDriver using PATH.
+   * ChromeDriver location is used to help find the chromedriver binary. This will be passed to the
+   * Selenium jar as the system property webdriver.chrome.driver. If the value is not set when
+   * launching locally, it will use the default values downloaded from webdriver-manager.
    *
    * example:
    * chromeDriver: './node_modules/webdriver-manager/selenium/chromedriver_2.20'
    */
   chromeDriver?: string;
+
+  /**
+   * geckoDriver location is used to help find the gecko binary. This will be passed to the Selenium
+   * jar as the system property webdriver.gecko.driver. If the value is not set when launching
+   * locally, it will use the default values downloaded from webdriver-manager.
+   */
+  geckoDriver?: string;
 
   // ---- 2. To connect to a Selenium Server which is already running ----------
 
