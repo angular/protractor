c9584c7931f9a60e1c5434a60c7362794f4e6d3c
Yi Qi <qiyi@google.com>
2017-11-30 16:46:56 -0800

    fix(jasmine): Update Jasmine to support Node8 async/await
    Breaking change for TypeScript:
    
    JasmineWD doesn't know anything about async/await, turns off JasmineWD if control flow was disabled.
    
    It will affect TypeScript tests that are using async/await and
    1. miss some await keyword in the test.(Previously, this might cause the
    test failed silently and be reported as pass), or
    2. use Promise in jasmine expect function
    
    Before
    ```ts
    await expect(getPromise()).toEqual(42);
    ```
    
    After
    ```ts
    expect(await getPromise()).toEqual(42);
    ```

--- spec/ts/plugin/plugin_spec.ts
+++ spec/ts/plugin/plugin_spec.ts
@@ -3,6 +3,6 @@ import {browser, protractor} from '../../..';
 describe('plugins', function() {
   it('should have run the onPageLoad hook', async function() {
     await browser.get('index.html');
-    await expect((protractor as any).ON_PAGE_LOAD).toBe(true);
+    expect(await (protractor as any).ON_PAGE_LOAD).toBe(true);
   });
 });
