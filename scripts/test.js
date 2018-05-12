e46083930c6548b5398424099035ef8a7d28b849
Craig Rodrigues <crodr001@gmail.com>
2018-05-12 14:05:49 -0700

    Remove tests which use elementExplorer, which is now removed.

--- scripts/test.js
+++ scripts/test.js
@@ -44,9 +44,6 @@ var passingTests = [
   'node built/cli.js spec/built/noCFPluginConf.js',
   //'node scripts/driverProviderAttachSession.js',
   'node scripts/errorTest.js',
-  // Interactive Element Explorer tasks
-  'node scripts/interactive_tests/interactive_test.js',
-  'node scripts/interactive_tests/with_base_url.js',
   // Unit tests
   'node node_modules/jasmine/bin/jasmine.js JASMINE_CONFIG_PATH=scripts/unit_test.json',
   // Dependency tests
