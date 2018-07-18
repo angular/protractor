c668d1c75b0bb9217bb22df74fda40f8b64d8d70
IgorSasovets <isasovets@gmail.com>
2018-07-18 22:45:53 +0300

    - added examples of usage protractor framework with angular-material components;

---
+++ example/angular_material/conf.js
@@ -0,0 +1,25 @@
+// An example configuration file.
+exports.config = {
+    directConnect: true,
+  
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+      'browserName': 'chrome'
+    },
+  
+    // Framework to use. Jasmine is recommended.
+    framework: 'jasmine',
+  
+    // Spec patterns are relative to the current working directory when
+    // protractor is called.
+    specs: [
+        'input_spec.js',
+        'mat_paginator_spec.js'
+    ],
+  
+    // Options to be passed to Jasmine.
+    jasmineNodeOpts: {
+      defaultTimeoutInterval: 30000
+    }
+  };
+  
