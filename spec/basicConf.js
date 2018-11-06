9c6af73880a63f5c85da7f90c16144e472d88a73
Craig Nishina <craig.nishina@gmail.com>
2018-11-06 10:29:43 -0800

    chore(test): move element_spec.js off of the control flow
    Update circleci to support async await.
    
    For the basicConf test suite:
    
    - Only run the element_spec test in the Protractor config, we will add
    back other specs as we migrate the basicConf off of the control
    flow.
    - In the Protractor configuration file, set `SELENIUM_PROMISE_MANAGER` to false.
    - Refactor to use async / await.
    - Refactor `var` to use either `const` or `let`.

--- spec/basicConf.js
+++ spec/basicConf.js
@@ -3,18 +3,19 @@ var env = require('./environment.js');
 // The main suite of Protractor tests.
 exports.config = {
   seleniumAddress: env.seleniumAddress,
+  SELENIUM_PROMISE_MANAGER: false,
 
   framework: 'jasmine',
 
   // Spec patterns are relative to this directory.
   specs: [
-    'basic/*_spec.js'
+    'basic/elements_spec.js'
   ],
 
   // Exclude patterns are relative to this directory.
-  exclude: [
-    'basic/exclude*.js'
-  ],
+  // exclude: [
+  //   'basic/exclude*.js'
+  // ],
 
   capabilities: env.capabilities,
 
