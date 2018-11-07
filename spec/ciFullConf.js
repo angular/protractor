eb08895efec0e7794d78ccb3ede50ba6c8d2b5e7
Craig Nishina <craig.nishina@gmail.com>
2018-11-07 11:48:59 -0800

    chore(test): move mockmodule_spec.js off of the control flow

--- spec/ciFullConf.js
+++ spec/ciFullConf.js
@@ -14,9 +14,9 @@ exports.config = {
     'basic/lib_spec.js',
     'basic/locators_spec.js'
     // 'basic/elements_spec.js',
-    // 'basic/navigation_spec.js',
     // 'basic/handling_spec.js'
-    // 'basic/elements_spec.js',
+    // 'basic/mockmodule_spec.js',
+    // 'basic/navigation_spec.js',
   ],
 
   // Exclude patterns are relative to this directory.
