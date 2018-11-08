e6075574e5d325d89f22ff2f7e939deca97227c4
Craig Nishina <craig.nishina@gmail.com>
2018-11-07 17:10:09 -0800

    chore(test): move withLogin test off of the control flow

--- spec/withLoginConf.js
+++ spec/withLoginConf.js
@@ -4,6 +4,7 @@ var env = require('./environment.js');
 // handle log-in using the onPrepare field.
 exports.config = {
   seleniumAddress: env.seleniumAddress,
+  SELENIUM_PROMISE_MANAGER: false,
 
   framework: 'jasmine',
 
@@ -15,20 +16,19 @@ exports.config = {
 
   baseUrl: env.baseUrl + '/ng1/',
 
-  onPrepare: function() {
-    browser.driver.get(env.baseUrl + '/ng1/login.html');
+  onPrepare: async() => {
+    await browser.driver.get(env.baseUrl + '/ng1/login.html');
 
-    browser.driver.findElement(by.id('username')).sendKeys('Jane');
-    browser.driver.findElement(by.id('password')).sendKeys('1234');
-    browser.driver.findElement(by.id('clickme')).click();
+    await browser.driver.findElement(by.id('username')).sendKeys('Jane');
+    await browser.driver.findElement(by.id('password')).sendKeys('1234');
+    await browser.driver.findElement(by.id('clickme')).click();
 
     // Login takes some time, so wait until it's done.
     // For the test app's login, we know it's done when it redirects to
     // index.html.
-    return browser.driver.wait(function() {
-      return browser.driver.getCurrentUrl().then(function(url) {
-        return /index/.test(url);
-      });
+    return await browser.driver.wait(async() => {
+      const url = await browser.driver.getCurrentUrl();
+      return /index/.test(url);
     }, 10000);
   }
 };
