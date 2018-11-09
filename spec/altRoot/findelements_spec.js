ea348ba9adc406b03bfc6c5b59584e1093baeb32
Craig Nishina <cnishina@users.noreply.github.com>
2018-11-08 17:03:38 -0800

    chore(test): move altRoot and inferRoot test off of the control flow (#5003)

--- spec/altRoot/findelements_spec.js
+++ spec/altRoot/findelements_spec.js
@@ -1,19 +1,19 @@
-describe('finding elements when ng-app is nested', function() {
-  beforeEach(function() {
-    browser.get('alt_root_index.html#/form');
+describe('finding elements when ng-app is nested', () => {
+  beforeEach(async() => {
+    await browser.get('alt_root_index.html#/form');
   });
 
-  it('should find an element by binding', function() {
-    var greeting = element(by.binding('{{greeting}}'));
+  it('should find an element by binding', async() => {
+    const greeting = element(by.binding('{{greeting}}'));
 
-    expect(greeting.getText()).toEqual('Hiya');
+    expect(await greeting.getText()).toEqual('Hiya');
   });
 
-  it('should find elements outside of angular', function() {
-    var outside = element(by.id('outside-ng'));
-    var inside = element(by.id('inside-ng'));
+  it('should find elements outside of angular', async() => {
+    const outside = element(by.id('outside-ng'));
+    const inside = element(by.id('inside-ng'));
 
-    expect(outside.getText()).toEqual('{{1 + 2}}');
-    expect(inside.getText()).toEqual('3');
+    expect(await outside.getText()).toEqual('{{1 + 2}}');
+    expect(await inside.getText()).toEqual('3');
   });
 });
