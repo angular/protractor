1f8e4595700ac2ff6568c61b6ef8171b7c9f090e
Oleksii <qacrispus@gmail.com>
2018-11-09 03:53:04 +0200

    chore(test): move expected_conditions_spec off of the control flow (#5013)

--- spec/ciFullConf.js
+++ spec/ciFullConf.js
@@ -14,6 +14,7 @@ exports.config = {
     'basic/lib_spec.js',
     'basic/locators_spec.js'
     // 'basic/elements_spec.js',
+    // 'basic/expected_conditions_spec.js',
     // 'basic/handling_spec.js'
     // 'basic/mockmodule_spec.js',
     // 'basic/navigation_spec.js',
