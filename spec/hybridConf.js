27838785e1ee6bbb9cc8b36aa197f14fa29c06a5
CrispusDH <qacrispus@gmail.com>
2018-11-09 10:54:45 +0200

    chore(test): move hybrid/async_spec off of the control flow

--- spec/hybridConf.js
+++ spec/hybridConf.js
@@ -1,8 +1,9 @@
-var env = require('./environment');
+const env = require('./environment');
 
 // This is the configuration for a smoke test for a hybrid ng1/ng2 application.
 exports.config = {
   seleniumAddress: env.seleniumAddress,
+  SELENIUM_PROMISE_MANAGER: false,
 
   framework: 'jasmine',
 
