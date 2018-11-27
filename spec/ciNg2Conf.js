e458025ad05a1547ad9e568bd6a55d421a3857bf
Craig Nishina <cnishina@users.noreply.github.com>
2018-11-27 09:13:58 -0800

    chore(promises): remove q promises and webdriver promises (#5052)
    - remove q promises and webdriver promises from the runner, launcher, plugins, and taskRunner
    - add deprecated message to element explorer.
    - add unhandledRejection
    - update browser versions used in travis tests

--- spec/ciNg2Conf.js
+++ spec/ciNg2Conf.js
@@ -1,21 +1,3 @@
-exports.config = require('./angular2Conf.js').config;
-
-exports.config.sauceUser = process.env.SAUCE_USERNAME;
-exports.config.sauceKey = process.env.SAUCE_ACCESS_KEY;
-exports.config.seleniumAddress = undefined;
-
-// TODO: add in firefox when issue #2784 is fixed
-exports.config.multiCapabilities = [{
-    'browserName': 'chrome',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'Protractor suite tests',
-    'version': '54',
-    'selenium-version': '2.53.1',
-    'chromedriver-version': '2.26',
-    'platform': 'OS X 10.11'
-  }];
-exports.config.capabilities = undefined;
-exports.config.allScriptsTimeout = 120000;
-exports.config.getPageTimeout = 120000;
-exports.config.jasmineNodeOpts.defaultTimeoutInterval = 120000;
+exports.config = require('./ciFullConf.js').config;
+exports.config.specs = require('./angular2Conf.js').config.specs;
+exports.config.exclude = undefined;
