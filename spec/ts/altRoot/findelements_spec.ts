4e420d971de6044840ab4e0ebd0490b66431bb70
CrispusDH <qacrispus@gmail.com>
2018-11-28 13:22:39 +0200

    migrate altRoot to ts

---
+++ spec/ts/altRoot/findelements_spec.ts
@@ -0,0 +1,20 @@
+import { browser, by, element } from '../../..';
+
+describe('finding elements when ng-app is nested', () => {
+  beforeEach(async() => {
+    await browser.get('alt_root_index.html#/form');
+  });
+
+  it('should find an element by binding', async () => {
+    const greeting = element(by.binding('{{greeting}}'));
+    expect(await greeting.getText()).toEqual('Hiya');
+  });
+
+  it('should find elements outside of angular', async () => {
+    const outside = element(by.id('outside-ng'));
+    const inside = element(by.id('inside-ng'));
+
+    expect(await outside.getText()).toEqual('{{1 + 2}}');
+    expect(await inside.getText()).toEqual('3');
+  });
+});
---
+++ spec/ts/altRootConf.ts
@@ -0,0 +1,22 @@
+import { Config } from '../..';
+const env = require('../environment.js');
+
+// Tests for an Angular app where ng-app is not on the body.
+export let config: Config = {
+  seleniumAddress: env.seleniumAddress,
+  SELENIUM_PROMISE_MANAGER: false,
+
+  framework: 'jasmine',
+
+  // Spec patterns are relative to this config.
+  specs: [
+    'altRoot/*_spec.js'
+  ],
+
+  capabilities: env.capabilities,
+
+  baseUrl: env.baseUrl + '/ng1/',
+
+  // Selector for the element housing the angular app.
+  rootElement: 'div#nested-ng-app'
+};
