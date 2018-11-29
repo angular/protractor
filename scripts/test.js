1e510453b1a5e0b37450e4941abd056b8635be90
Craig Nishina <cnishina@users.noreply.github.com>
2018-11-29 00:29:51 -0800

    clean up merge conflict

--- scripts/test.js
+++ scripts/test.js
@@ -43,17 +43,17 @@ const passingTests = [
   'node built/cli.js spec/built/noCFBasicConf.js',
   'node built/cli.js spec/built/noCFBasicConf.js --useBlockingProxy',
   'node built/cli.js spec/built/noCFPluginConf.js',
-  // //'node scripts/driverProviderAttachSession.js',
+  'node scripts/driverProviderAttachSession.js',
   // 'node scripts/errorTest.js',
   // // Interactive Element Explorer tasks
   // 'node scripts/interactive_tests/interactive_test.js',
   // 'node scripts/interactive_tests/with_base_url.js',
   // Unit tests
   'node node_modules/jasmine/bin/jasmine.js JASMINE_CONFIG_PATH=scripts/unit_test.json',
-  // // Dependency tests
-  // 'node node_modules/jasmine/bin/jasmine.js JASMINE_CONFIG_PATH=scripts/dependency_test.json',
-  // // Typings tests
-  // 'node spec/install/test.js'
+  // Dependency tests
+  'node node_modules/jasmine/bin/jasmine.js JASMINE_CONFIG_PATH=scripts/dependency_test.json',
+  // Typings tests
+  'node spec/install/test.js'
 ];
 
 const executor = new Executor();
