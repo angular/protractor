1aac1ca83f1a21b0abccdd236373c5705f3bf44f
Amy Richards <arichards@factset.com>
2019-03-25 08:36:32 -0400

    feat(config): support async config
    Allows protractor config file export to be a
    promise that resolves to a config object instead
    of only allowing a config object.
    
    Closes #5148
    Closes #4749

--- lib/runnerCli.ts
+++ lib/runnerCli.ts
@@ -9,7 +9,7 @@ import {Runner} from './runner';
 
 let logger = new Logger('runnerCli');
 
-process.on('message', (m: any) => {
+process.on('message', async (m: any) => {
   switch (m.command) {
     case 'run':
       if (!m.capabilities) {
@@ -18,7 +18,7 @@ process.on('message', (m: any) => {
       // Merge in config file options.
       let configParser = new ConfigParser();
       if (m.configFile) {
-        configParser.addFileConfig(m.configFile);
+        await configParser.addFileConfig(m.configFile);
       }
       if (m.additionalConfig) {
         configParser.addConfig(m.additionalConfig);
