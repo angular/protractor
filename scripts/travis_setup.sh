6baf21881d2cbcd217d5f2ba83aab8c857327db1
Keen Yee Liau <kyliau@google.com>
2020-04-15 10:26:45 -0700

    ci: Log sauce connect proxy to stdout, remove travis_wait, upgrade proxy to 4.5.4
    travis_wait is not needed if the sauce connect proxy logs are written to
    stdout. This makes debugging proxy problems from the CI logs much
    easier. This commit also updates the debugging command to use the
    `--doctor` flag.
    
    Other changes include:
    1. Upgrade sauce-connect proxy to 4.4.1 to 4.5.4

--- scripts/travis_setup.sh
+++ scripts/travis_setup.sh
@@ -1,6 +1,8 @@
 if [ $JOB == "bstack" ]; then
+  echo "Setting up Browser Stack"
   ./scripts/browserstack_local_setup.sh
 else
+  echo "Setting up Sauce Labs"
   ./scripts/sauce_connect_setup.sh
   ./scripts/wait_for_browser_provider.sh
 fi
